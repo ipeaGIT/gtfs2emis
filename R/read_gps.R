96768a12b320a25427168019627cb823a38ca510
Joao Pedro Bazzo Vieira <B138750230@ipea.gov.br>
2019-11-08 16:53:54 -0300

    improve read_gps performance

--- R/read_gps.R
+++ R/read_gps.R
@@ -3,27 +3,25 @@
 #
 # data import
 # 
-# filepath <- "L:/# DIRUR #/ASMEQ/bosistas/joaobazzo/gtfs2gps/tests_joao/data/output/gtfs_spo_sptrans_2019-10/"
 read_gps <- function(filepath){
   dt <- data.table::fread(filepath)
   # stop id
-  id0 <- which(!is.na(dt$stop_sequence))
+  id0 <- which(!is.na(dt$stop_sequence)); id0[1] <- 1
   id1 <- data.table::shift(id0,-1,fill=nrow(dt))
+  list_ids <- lapply(seq_along(id0),function(i){id0[i]:id1[i]})
   # exclude single counts
   #dtf <- list()
-  dtf <- lapply(1:length(id0),function(i){
-    list_ids <- id0[i]:id1[i]
-    sf::st_sf(dt[list_ids,
-             c("trip_id","id","departure_time","stop_id","stop_sequence","dist","speed")][1],
-          geometry=sf::st_sfc(st_linestring(x=as.matrix(dt[list_ids,
-                                                       c("shape_pt_lon","shape_pt_lat")])),crs=4326))
+  dtf <- lapply(list_ids,function(i){
+    sf::st_sf(dt[i,
+                 c("trip_id","id","departure_time","stop_id","stop_sequence","speed")][1],
+              geometry=sf::st_sfc(st_linestring(x=as.matrix(dt[i,
+                                                               c("shape_pt_lon","shape_pt_lat")])),crs=4326))
   }) %>% 
     data.table::rbindlist() 
-  
-  
-  dtf <- dtf[which(as.numeric(st_length(dtf$geometry))>0),]
-  dtf <- dtf[-which(as.numeric(st_length(dtf$geometry))==
-                     max(as.numeric(st_length(dtf$geometry)))),]
-  dtf$dist <- set_units(st_length(dtf$geometry),"km")
+
+  dtf <- dtf[as.numeric(st_length(geometry))>0,][as.numeric(as.numeric(st_length(geometry)))<
+                                                                 max(as.numeric(st_length(geometry))),]
+
+  dtf[,dist := units::set_units(st_length(dtf$geometry),"km")]
   return(dtf)
 }
