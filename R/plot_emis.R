ca6ffc75d130cb46357379fffb32c53f85ec5496
Joao Pedro Bazzo Vieira <B138750230@ipea.gov.br>
2019-11-08 16:55:01 -0300

    here's the status

--- R/plot_emis.R
+++ R/plot_emis.R
@@ -9,7 +9,6 @@ library(cptcity)
 library(sf)
 library(units)
 library(mapview)
-library(h3jsr)
 library(plyr)
 library(geobr)
 library(tibble)
@@ -19,34 +18,19 @@ setwd("L:/# DIRUR #/ASMEQ/bosistas/joaobazzo/gps2emission/")
 # data import
 filepath <- "data/emi_grid//"
 ids <- list.files(path=filepath,pattern = ".shp");ids_saida <- str_remove(ids,".shp")
-hex_grid <- readRDS("data/hex/3550308_09.rds") %>% st_transform(31983) %>% st_sf()# hex reading
-# export couple files
-all_hex <- lapply(3:10, function(i){
-  # read
-  dt <- sf::read_sf(paste0("data/emi/",ids[i]))%>% st_transform(31983)
-  # intersect
-  its <- sf::st_intersects(dt$geometry,hex_grid$geometry) %>% as.data.table()
-  colnames(its) <- c("emi_id","hex_id")
-  its$hex_id <- hex_grid$h3_address[its$hex_id]
-  hex_city <- hex_grid[hex_grid$h3_address %in% unique(its$hex_id),]
-  hex_city$emi <- emis_grid(spobj = dt["emissns"],g = hex_city)$emissns
-  # salve
-  sf::write_sf(hex_city,paste0("data/emi_grid/",ids[i]))
-  return(hex_city)
-}) %>% data.table::rbindlist() %>% st_as_sf()
-head(all_hex)
-#
+break()
 # plotting
-dd <- future.apply::future_lapply(1:2,function(i){
+system.time({
+dd <- future.apply::future_lapply(1:length(ids),function(i){
   sf::read_sf(paste0("data/emi_grid/",ids[i]))%>% st_transform(31983)
 }) %>% data.table::rbindlist() 
-dd
+})
 #dd %>% data.table::as.data.table() #%>% st_as_sf()
 dd <- dd[,emi:=sum(emi),by=h3_ddrs][,.SD[1],by=h3_ddrs] %>% st_as_sf()
-mapview(dd["emi"])
+sync(mapview(dd["emi"]),mapview(dd$geometry,alpha.regions = 0))
 #d1 <- dd[dd$h3_ddrs %in% unique(dd$h3_ddrs),]
 dd$emi1 %>% sum()
-mapview(dd$geometry)
+mapview(dd$geometry[1],color="red",alpha.regions = 0)
 mapview(dd["h3_rslt"])
 plot(dd["geometry"])
 # intersect
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # gps2emission
 Estimation of bus emissions based on GPS data
+
+## to do
+- generate low gps resolution for `gtfs2gps` data set from SP, Fortaleza and Porto Alegre
+- verify bus fleet on each city
+
+## doing
+- first results for emissions in SP, considering speed variation
+- verifying code for plotting maps
+
+## done
+- first results for emission in SP, without speed consideration
+  - emissions seems to be concentrated on bus terminals
+  - 18h30 min to estimate emissions
+  - the main reason for slow processing time is the high size of input data (gps for each `15` min)
