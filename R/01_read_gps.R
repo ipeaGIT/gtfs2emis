031ec1e0ef0e9a4b5940e71456561ba87556ce15
joao <joao.bazzo@gmail.com>
2019-12-17 16:01:55 -0300

    data.table function improvement2

--- R/01_read_gps.R
+++ R/01_read_gps.R
@@ -3,69 +3,8 @@
 #
 # data import
 # 
+#
 read_gps <- function(filepath){
-  
-  dt <- data.table::fread(filepath)
-  # stop id
-  id0 <- which(!is.na(dt$stop_sequence)); id0[1] <- 1
-  id1 <- data.table::shift(id0,-1,fill=nrow(dt))
-  list_ids <- lapply(seq_along(id0),function(i){id0[i]:id1[i]})
-  # exclude single counts
-  #dtf <- list()
-  dtf <- lapply(list_ids,function(i){
-    sf::st_sf(dt[i,
-                 c("trip_id","id","departure_time","stop_id","stop_sequence","speed")][1],
-              geometry=sf::st_sfc(st_linestring(x=as.matrix(dt[i,
-                                                               c("shape_pt_lon","shape_pt_lat")])),crs=4326))
-  }) %>% 
-    data.table::rbindlist() 
-
-  dtf <- dtf[as.numeric(st_length(geometry))>0,][as.numeric(as.numeric(st_length(geometry)))<
-                                                                 max(as.numeric(st_length(geometry))),]
-
-  dtf[,dist := units::set_units(st_length(dtf$geometry),"km")]
-  return(dtf)
-}
-#
-read_gps01 <- function(filepath){
-  dt <- data.table::fread(filepath)
-  dt[,id := 1:nrow(dt)]
-  # --
-  # trip division
-  # --
-  mdist1 <- which(dt$cumdist == max(dt$cumdist))
-  mdist0 <- c(1,head(mdist1,-1)) 
-  list_dist <- lapply(seq_along(mdist0), function(i){data.table(range = i,id = mdist0[i]:mdist1[i])}) %>%
-    rbindlist()
-  dt[list_dist,on= "id",range_trip := i.range]  # add range
-  # --
-  # stop division
-  # --
-  id0 <- c(1,which(!is.na(dt$stop_sequence))) 
-  id1 <- c(id0[-1],nrow(dt))
-  list_ids <- lapply(seq_along(id0),function(i){data.table(range = i,id = id0[i]:id1[i])}) %>% 
-    rbindlist()
-  dt[list_ids,on= "id",range_id := i.range]  # add range
-  # --
-  # first change
-  # --
-  dt1 <- dt[,.SD[1],by=.(range_trip,range_id)]
-  setcolorder(dt1,names(dt))
-  dt1[,c("range_id","id"):= list(range_id-1,id-0.1)][-c(1,.N),]
-  dt <- rbindlist(list(dt,dt1))[order(id)]
-  #
-  dt1 <- dt[,{
-    geometry <- sf::st_linestring(x=matrix(c(shape_pt_lon,shape_pt_lat),ncol=2)) %>% 
-    sf::st_sfc() 
-    aux_dt <- data.table(trip_id,id,departure_time,stop_id,stop_sequence,speed)[1,]
-    sf::st_sf(aux_dt,geometry=geometry,crs = 4326)
-  },
-  by = .(range_trip,range_id)][,"geometry"]
-
-  return(dt1)
-}
-#
-read_gps02 <- function(filepath){
   dt <- data.table::fread(paste0(filepath))
   dt[,id := 1:nrow(dt)]
   # --
