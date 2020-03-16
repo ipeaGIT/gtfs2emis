4a3eed166d238bca476a37d308c2d299304f7a6e
joao <joao.bazzo@gmail.com>
2020-03-16 17:09:12 -0300

    prep gtfs2gps

---
+++ prep/01_prep_gtfs2gps.R
@@ -0,0 +1,25 @@
+create_gps_outputs <- function(city_abrev, day_start, day_end){
+  
+  # city_abrev = 'ade'
+  message(paste0('Working on city: ', city_abrev,', ', which(proj_cities$abrev_city == city_abrev)), ' out of ', nrow(proj_cities))
+  
+  
+  # get raw gtfs
+  f <- list.files("../data-raw/gtfs/", pattern = city_abrev)
+  gtfs_file <- paste0("../data-raw/gtfs/", f)
+  
+  # read gtfs to memory
+  gtfs <- gtfs2gps::read_gtfs(gtfs_file)
+  
+  # filter time of the day and weekdays
+  gtfs <- gtfs2gps::filter_week_days(gtfs)
+  gtfs <- gtfs2gps::filter_day_period(gtfs, period_start = day_start, period_end = day_end)
+  
+  # convert gtfs to gps-like format
+  gps_df <- gtfs2gps::gtfs2gps(gtfs, spatial_resolution = 50, progress = F)
+  gps_sf <- gtfs2gps::gps_as_sf(gtfs)
+  
+  # save outputs
+  return(gps_sf)
+  
+}
