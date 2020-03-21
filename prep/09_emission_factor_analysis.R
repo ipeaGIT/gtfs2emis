6af46927ada87614bb19d116af6319963ead158b
joao.bazzo@gmail.com <joao.bazzo@gmail.com>
2020-03-21 08:46:28 -0300

    visualization of EF(1)

--- prep/09_emission_factor_analysis.R
+++ prep/09_emission_factor_analysis.R
@@ -20,22 +20,67 @@ buses_type_aut <- unique(ef$Segment)[1:3]
 buses_type_vein <- c("Midi","Std","RT")
 g_vein <- c( "<=15",">15 & <=18",">18")
 euro_vein <- c("PRE", "I", "II", "III", "IV", "V")
-euro_aut <- c("Conventional","Euro I","Euro II","Euro III","Euro IV","Euro V")
+#euro_aut <- c("Conventional","Euro I","Euro II","Euro III","Euro IV","Euro V")
+# tech_aut <- c(rep(NA,4),"SCR","SCR")
+euro_aut <- c("Euro I","Euro II","Euro III","Euro IV","Euro V")
+tech_aut <- c(rep(NA,3),"SCR","SCR")
+pol_aut <- c("CO","NOx","VOC","PM Exhaust")
+slope_aut <- c(-0.06,0,0.06)
+speed <- 1:100  
+
+ef[Pollutant %in% "CO",][,unique(Technology),by = .(Segment, Euro.Standard)] #[,.SD[1],by = .(Segment,Euro.Standard)]
+
+lapply(1:length(buses_type_aut),function(b){
+  # b = 1
+  df2 <- lapply(1:length(euro_aut),function(e){
+    # e = 1
+    df1 <- lapply(1:length(pol_aut), function(p){
+      # p = 1
+      df0 <- lapply(1:length(slope_aut), function(s){
+        df <- data.frame("speed" = as.numeric(speed),
+                         "ef" = ef_hdv_speed_2019(vel = speed,ef = ef,
+                                                  veh = "Buses",fuel = "Diesel",
+                                                  segment = buses_type_aut[b], 
+                                                  euro = euro_aut[e],
+                                                  tech = tech_aut[e],
+                                                  pol = pol_aut[p],
+                                                  slope = slope_aut[s],
+                                                  load = 0.5),
+                         "slope" = as.character(slope_aut[s]),
+                         "euro" = euro_aut[e], 
+                         "pol" = pol_aut[p])
+        return(df)
+      }) %>% data.table::rbindlist()
+      return(df0)
+    }) %>% data.table::rbindlist()
+    return(df1)
+  }) %>% data.table::rbindlist()
+  pp <- ggplot(data = df2,aes(x = speed,y = as.numeric(ef), group = slope)) + 
+    geom_line(aes(color = euro,linetype = slope))+
+    scale_linetype_manual(values=c("twodash", "solid","dotted"))+
+    #geom_line(aes(linetype = slope,color = euro)) +
+    facet_grid(cols = vars(euro),rows = vars(pol), scales = "free_y") +
+    ylab("FE(v) (g/km)") + xlab("Velocidade (km/h)")+ xlim(10,100) +
+    scale_fill_discrete(guide = FALSE) + labs(color = buses_type_aut[b], linetype = "Slope (%)")
+
+  # ----
+  nmfile <- paste0("figures/ef_",buses_type_vein[b],".jpg")
+  ggsave(filename = nmfile,plot = pp,width = 18,height = 8,units = "cm",scale = 1.2,dpi = 300)
   
-e = 6; i = 1
+})
 #sapply(1:nrow(buses_type), function(i){
-  message(buses_type[i])
+message(buses_type[i])
 vein::ef_hdv_speed(v = "Ubus",
                    speed = vein::Speed(86,"km/h"),
-                              t = buses_type_vein[i],
-                              g = g_vein[i],
-                              eu = euro_vein[e],
-                              gr = 0,
-                              l = 1.0,
-                              p = "CO",
-                              show.equation = FALSE) 
-  
-  ef_hdv_speed_2019(vel = 86,
+                   t = buses_type_vein[i],
+                   g = g_vein[i],
+                   eu = euro_vein[e],
+                   gr = 0,
+                   l = 1.0,
+                   p = "CO",
+                   show.equation = FALSE) 
+
+ef_hdv_speed_2019(vel = 86,
                   ef = ef,
                   veh = "Buses",
                   fuel = "Diesel",
@@ -45,16 +90,16 @@ vein::ef_hdv_speed(v = "Ubus",
                   pol = "CO",
                   slope = 0,
                   load = 0.5)
-  ef[Category %in% veh & 
-       Fuel %in% "Diesel" & 
-       Segment %in% buses_type[i] & 
-       Euro.Standard %in% euro_aut[e] & 
-       Technology %in% "EGR" &
-       Pollutant %in% "CO" &
-       Road.Slope %in% 0 &
-       #Mode %in% mode &
-       Load %in% 1.0,`EF.[g/km].or.ECF.[MJ/km]`]
-  
+ef[Category %in% veh & 
+     Fuel %in% "Diesel" & 
+     Segment %in% buses_type[i] & 
+     Euro.Standard %in% euro_aut[e] & 
+     Technology %in% "EGR" &
+     Pollutant %in% "CO" &
+     Road.Slope %in% 0 &
+     #Mode %in% mode &
+     Load %in% 1.0,`EF.[g/km].or.ECF.[MJ/km]`]
+
 })
 
 
