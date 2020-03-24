7443f612a38ba1f5535963dba31009c0a75e3136
joao.bazzo@gmail.com <joao.bazzo@gmail.com>
2020-03-24 18:34:43 +0000

    update analysis of Curitiba fleet per shape_id

--- prep/01.01_download_gps_data_cur.R
+++ prep/01.01_download_gps_data_cur.R
@@ -7,6 +7,12 @@ for(i in 1:length(data)){
                 quiet = FALSE, mode = "w")
   message(paste0("download: ", file))
 }
-
+for(i in 1:length(data)){
+  cdg_url <- paste0("http://dadosabertos.c3sl.ufpr.br/curitibaurbs/",data[i],"_shapeLinha.json.xz")
+  file <- paste0(data[i],"_shapeLinha.json.xz")
+  download.file(cdg_url, destfile = paste0("../../data-raw/gps/cur/",file), 
+                quiet = FALSE, mode = "w")
+  message(paste0("download: ", file))
+}
 
 
--- prep/01.1_prep_cur_fleet.R
+++ prep/01.1_prep_cur_fleet.R
@@ -1,44 +1,43 @@
 #
 # ---------------
-library(openxlsx)
-library(rjson)
-library(jsonlite)
-library(dplyr)
-library(data.table)
-library(gtfs2gps)
 rm(list=ls())
 #
 # leitura tipo de frota
 #
 df <- openxlsx::read.xlsx("../../data-raw/fleet/cur_frota/URBS_plan.xlsx") %>% 
   data.table::as.data.table()
-#
-# leitura gtfs
-#
-gtfs <- gtfs2gps::read_gtfs("../../data-raw/gtfs/gtfs_BRA_cur_201910.zip")
 # 
-# leitura dado de GPS
+# leitura dado de GPS (tabelaVeiculo)
 #
 veh_folders = list.files("../../data-raw/gps/cur/",pattern = "tabelaVeiculo.json.xz",full.names = TRUE)
-veh_acum <- lapply(1:length(veh_folders),function(i){ # i = 1
+veh_acum <- lapply(1:length(veh_folders),function(i){ # i = 33
   message(i)
   veic <- jsonlite::fromJSON(file(veh_folders[i])) %>% data.table::as.data.table()
   veic <- veic[,.SD[1],by = .(COD_LINHA,VEICULO)]
   return(veic)
 }) %>% data.table::rbindlist()
-veh_acum <- veh_acum[,.SD[1],by = .(COD_LINHA,VEICULO)][,.(COD_LINHA,VEICULO)]
+veh_acum1 <- veh_acum[,.SD[1],by = .(COD_LINHA,VEICULO)][,.(COD_LINHA,VEICULO)]
+# 
+# leitura dado de GPS (shapeLinha)
+#
+shp_folders = list.files("../../data-raw/gps/cur/",pattern = "shapeLinha.json.xz",full.names = TRUE)
+shp_acum <- lapply(1:length(shp_folders),function(i){ # i = 1
+  message(i)
+  shp <- jsonlite::fromJSON(file(shp_folders[i])) %>% data.table::as.data.table()
+  shp <- shp[,.SD[1],by = .(SHP,COD)]
+  return(shp)
+}) %>% data.table::rbindlist()
+shp_acum1 <- shp_acum[,.SD[1],by = .(SHP,COD)][,.(SHP,COD)]
 #
 # join
 #
-veiculos4 <- df[veh_acum,on = c("Prefixo" = "VEICULO")]
-#
-# associate 'shape_id' with 'route_short_name'
-#
-veiculos5 <- veiculos4[-which(is.na(veiculos4$Data)),]
-veiculos5[gtfs$routes,on = c('COD_LINHA' = 'route_short_name'),route_id := i.route_id]
-veiculos5[gtfs$trips,on = c('route_id' = 'route_id'),shape_id := i.shape_id]
-veiculos5 <- veiculos5[!is.na(veiculos5$shape_id),]
+veh <- lapply(1:max(table(shp_acum1$COD)),function(i){ # i = 8
+  aux_shp <- shp_acum1[,.SD[i],by = COD][!is.na(SHP),]
+  la <- veh_acum1[aux_shp,on = c("COD_LINHA" = "COD"),SHP := i.SHP][!is.na(SHP),][VEICULO != "",]
+  return(la)
+}) %>% data.table::rbindlist()
+df3 <- veh[df,on = c("VEICULO" = "Prefixo")][!is.na(COD_LINHA),][,.SD[1],by = .(COD_LINHA,VEICULO,SHP)]
 #
 # write
 #
-readr::write_rds(veiculos5,"../../data/fleet/cur/cur.rds")
+readr::write_rds(df3,"../../data/fleet/cur/cur.rds")
