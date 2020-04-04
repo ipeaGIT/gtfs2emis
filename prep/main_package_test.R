f799ad425b9e729fc1ff0b650b46f2be97e6500f
Joaobazzo <Joao.bazzo@gmail.com>
2020-04-04 16:37:48 -0300

    setdtthreads

--- prep/main_package_test.R
+++ prep/main_package_test.R
@@ -6,12 +6,17 @@ devtools::load_all()
 devtools::document() # it creates a man, updates namespace
 devtools::check()
 devtools::install()
+
+data.table::setDTthreads(threads = 1)
+ 
 # ----
 # 
-ef_hdv_speed(vel = units::set_units(10,km/h),veh = "Urban Buses Midi <=15 t",
+ef_hdv_speed(vel = 10,veh = "Urban Buses Midi <=15 t",
              fuel = "Diesel",euro = "Euro III",tech = NA,pol = "CO",show.equation = TRUE)
 
 gtfs2gps(raw_gtfs = "inst/extdata/gtfs_cur.zip",filepath = "test_joao/gps/",filter_weekdays = TRUE)
 
-gps_to_linestring(input_filepath = "test_joao/gps/",output_filepath = "test_joao/lines/",
-                  fleet_data = "inst/extdata/cur_fleet.tar.xz",overwrite = FALSE)
+gps2line(input_filepath = "test_joao/gps/",output_filepath = "test_joao/lines/",
+                  fleet_path = "inst/extdata/cur_fleet.tar.xz",overwrite = TRUE)
+
+emis(pol_list = c("CO","NOx"),input_folder = "test_joao/lines",output_folder = "test_joao/emis",overwrite = FALSE)
