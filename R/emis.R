023cda4a4044741913506f62ddc8c97ec7dc0c65
Joaobazzo <Joao.bazzo@gmail.com>
2020-05-11 11:00:31 -0300

    update emis for more than one pol and veh

--- R/emis.R
+++ R/emis.R
@@ -1,96 +1,60 @@
 #' @title Estimate hot exhaust emissions 
 #'
-#' @description Transform intervals of GPS data of constant speed into
-#' a linestring segment, in order to reduce data size of observations.
-#' In Curitiba, for the sake of simplicity, I'm adding:
-# a) "Hybric" vehicles as "Buses Standard"
-# b) "Biarticulated" as "Articulated"
+#' @description Calculate hot exhaust emissions
 #'
-#' @param pol_list Character; Vector of list of pollutants
-#' @param input_folder Character;
-#' @param output_folder Character;
-#' @param overwrite Logical;
-#' 
+#' @param veh Vehicles data-frame or list of "Vehicles" data-frame.
+#' Each data-frame as number of columns matching the age distribution of that ype of vehicle. The number of rows is equal to the number of streets link. If this is a list, the length of the list is the vehicles for each hour.
+#' @param dist Units (in km); Length of each link in km
+#' @param ef Units (in g/km);	List of functions of emission factors
+#' @return Units (in g); emissions per link
+#' @examples 
+#' vehdt <- data.table::data.table("veh1" = 0.75,"veh2" = 0.25)
+#' efdt <- list("CO"=units::set_units(c(0.25,0.17),'g/km'))
+#' distdt <- units::set_units(rep(1,10),'km')
+#' emis(veh = vehdt, dist = distdt, ef = efdt)
 #' @export
-emis <- function(pol_list, input_folder, output_folder = ".",overwrite = TRUE){
+emis <- function(veh,dist,ef){
+  # dist <- units::set_units(rep(2,10),"km")
+  # ef <- list("CO"=set_units(rep(1,5),'g/km'),"MP" = set_units(rep(-1,5),'g/km'))
+  # veh <- data.table("veh1" = 0.2,"veh2" = 0.2,"veh3" = 0.2,"veh4" = 0.2,"veh5" = 0.2)
+  # veh <- vehdt
+  # check units
   #
-  # input_folder = "test_joao/lines"
-  # output_folder = "test_joao/emis"
-  # pol_list = c("CO","NOx")
-  # emission_factor = ef
-  #
-  # files in input_folder
-  gps_line <- list.files(input_folder,recursive = FALSE)
-  #gps_line_names <- list.files(input_folder,recursive = FALSE,full.names = FALSE) %>% 
-  #  gsub(".txt","",gps_line[i])
-  #
-  # check existing files in output_folder and output 'gps_line'
-  # files
-  #
-  check_files <- paste0(input_folder,"/",list.files(output_folder,recursive = TRUE))
-  #check_files_names <- stringr::str_remove_all(list.files(output_folder),".txt")
-  if(length(check_files) > 1 & overwrite == FALSE){
-    gps_line <- gps_line[gps_line %nin% list.files(output_folder)]
+  # dist
+  if(class(dist) != "units"){
+    stop("dist neeeds to has class 'units' in 'km'. Please, check package 'units'")
   }
-  # euro equivalence
-  euro_eq <- data.table::data.table("euro_vein" = c(rep("I",4),rep("II",5),rep("III",6),rep("IV",3),rep("V",8)),
-                                    "euro" = c(rep("Euro I",4),rep("Euro II",5),rep("Euro III",6),rep("Euro IV",3),rep("Euro V",8)),
-                                    "ano" = c(1994:2019))
-  # loop per line
-  lapply(seq_along(gps_line),function(i){ # i = 1
-    # introduction message
-    message(paste0("shape_id #",gsub(".txt","",gps_line[i])," range ",i,"/",length(gps_line)))
-    # read line
-    dtline <- data.table::fread(paste0(input_folder,"/",gps_line[i]))
-    dtline[,dist := units::set_units(dist,km)]
-    #
-    # check fleet completeness
-    if(is.na(dtline$frota_ano[1])){
-      message(paste0("shape_id #",gsub(".txt","",gps_line[i])," without fleet data"))
-      return(NULL)
-    }else{
-      # fleet
-      if(unique(dtline$tipo_de_veiculo) %in% c("SEMI PADRON","PADRON","COMUM","HIBRIDO BIO","HIBRIDO")){
-        veh_type = "BUS_URBAN_D" # for CETESB emission factor data base
-        veh_segment = "Urban Buses Standard 15 - 18 t"
-        veh_vein_type = "Std"
-        veh_gweight = ">15 & <=18"
-      }
-      if(unique(dtline$tipo_de_veiculo) %in% c("MICRO ESPECIAL","MICRO")){
-        veh_type = "BUS_MICRO_D" # for CETESB emission factor data base
-        veh_segment = "Urban Buses Midi <=15 t"
-        veh_vein_type = "Midi"
-        veh_gweight = "<=18"
-      }
-      if(unique(dtline$tipo_de_veiculo) %in% c("ARTICULADO","ARTIC. BIO","BIARTICULADO","BIARTIC. BIO")){
-        veh_type = "BUS_ARTIC_D"  # for CETESB emission factor data base
-        veh_segment = "Urban Buses Articulated >18 t"
-        veh_vein_type = "TT"
-        veh_gweight = ">18"
-      }
-      
-      lapply(pol_list,function(pol){ # pol = pol_list[1]
-        # emission factor
-        FE_local <- vein::ef_cetesb(p = pol,
-                                    veh = veh_type,
-                                    year = unique(dtline$frota_ano),
-                                    agemax = 1)
-        FE_speed <- ef_hdv_scaled(dfcol = FE_local,vel = dtline$speed,veh =  veh_segment,fuel = "Diesel",
-                                  euro = euro_eq[ano %in% unique(dtline$frota_ano),euro],
-                                  pol = pol,show.equation = FALSE)
-        # FE_vein <- vein::ef_hdv_scaled(dfcol = FE_local,v = "Ubus",t = veh_vein_type,g = veh_gweight,
-        #                                eu = euro_eq[ano %in% unique(dtline$frota_ano),euro_vein],
-        #                                p = pol)[[1]](dtline$speed)
-        # allocate into data.table frame
-        dtline[,paste0("EM_",pol) := FE_speed * dist]
-        return(NULL)
-      })
+  if(units(dist)$numerator != "km"){
+    stop("dist need to has 'units' in 'km'.")
+  }
+  # check ef units
+  if(class(ef) != "list"){
+    stop("ef should be presented in 'list' class. Please, try using 'list(ef)' instead.")
+  }
+  for(i in seq_along(ef)){ # i = 1
+    if(class(ef[[i]]) != "units"){
+      stop("ef neeeds to has class 'units' in 'g/km'. Please, check package 'units'")
     }
-    
-    #
-    # save file
-    #
-    data.table::fwrite(dtline,paste0(output_folder,"/",unique(dtline$shape_id),".txt"))
-    return(NULL)
-  })
+    if(units(ef[[i]])$numerator != "g" | units(ef[[i]])$denominator != "km"){
+      stop("ef need to has 'units' in 'g/km'.")
+    }
+  }
+
+   # disagregated emissions
+  emi <- lapply(seq_along(list(ef)),function(i){ # i = 1 
+    # ef into matrix (efm)
+    efm <- matrix(ef[[i]])
+    # check length  
+    if(nrow(efm) != length(veh)){
+      stop("Length of ef is different than veh. \n Lengths should be the same.")
+    }
+    # estimate emissions
+    aux <- efm * as.numeric(veh)
+    temp_dist <- as.numeric(dist)
+    temp_emis <- aux %*% t(matrix(temp_dist))
+    row.names(temp_emis) <- names(veh)
+    return(temp_emis)
+  }) 
+  names(emi) <- names(ef)
+  return(emi)
 }
