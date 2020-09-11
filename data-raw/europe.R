aca5d398bf31d1c20483cb0b2a89768253281d8c
Joao Bazzo <joao.bazzo@gmail.com>
2020-09-11 15:48:03 -0300

    add link on script for data(europe)

--- data-raw/europe.R
+++ data-raw/europe.R
@@ -5,13 +5,15 @@ library(magrittr)
 #
 # https://www.eea.europa.eu/publications/emep-eea-guidebook-2019/part-b-sectoral-guidance-chapters/1-energy/1-a-combustion/road-transport-appendix-4-emission/view
 #
-europe <- openxlsx::read.xlsx("../gtfs2emis/test_joao/references/copert/1.A.3.b.i-iv Road transport hot EFs Annex 2018_Dic.xlsx") %>% 
-  data.table::as.data.table()
+filepath <- "https://www.eea.europa.eu/publications/emep-eea-guidebook-2019/part-b-sectoral-guidance-chapters/1-energy/1-a-combustion/road-transport-appendix-4-emission/at_download/file"
+
+europe <- openxlsx::read.xlsx(xlsxFile = filepath, sheet = 2)
+europe <- data.table::as.data.table(europe)
 europe <- europe[Category %in% "Buses",]
 europe <- europe[Pollutant %in% "PM Exhaust",Pollutant := "PM"]
 europe <- europe[Mode %in% "", Mode := NA]
 europe <- europe[Technology %in% "", Technology := NA]
 europe <- europe[,`EF.[g/km].or.ECF.[MJ/km]`:= NULL]
-#data.table::fwrite(europe,"data/europe.txt")
+names(europe)
 
 usethis::use_data(europe,overwrite = TRUE)
