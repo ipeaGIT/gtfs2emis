df5e2548ee145d014925d136d176969bd6d27835
Joao Bazzo <joao.bazzo@gmail.com>
2020-10-02 15:04:18 -0300

    change names europe webscrapping

--- data-raw/europe.R
+++ data-raw/europe.R
@@ -10,7 +10,7 @@ library(data.table)
 #    1.A.3.b.i, 1.A.3.b.ii, 1.A.3.b.iii, 1.A.3.b.iv
 #    Passenger cars, light commercial trucks, heavy-duty vehicles including
 #    buses and motor cycles
-# 2) EMEP/EEA for 2016 and 2013 were also used to add Fuel Consumption 
+# 2) EMEP/EEA for 2013 and 2009 were also used to add Fuel Consumption 
 # and CO2 emissions factor
 #
 # download in --------------------
@@ -47,19 +47,19 @@ emep2019[,k := 1]
 emep2019[,Thita := 0]
 data.table::setnames(emep2019,old = "Reduction.Factor.[%]", new = "RF")
 # ---
-# read 2016 ------------------------
+# read 2013 ------------------------
 # ---
 
-link2016 <- "https://www.eea.europa.eu/publications/emep-eea-guidebook-2013/part-b-sectoral-guidance-chapters/1-energy/1-a-combustion/1-a-3-b-road-transport-annex-hdv-files.zip/at_download/file"
+link2013 <- "https://www.eea.europa.eu/publications/emep-eea-guidebook-2013/part-b-sectoral-guidance-chapters/1-energy/1-a-combustion/1-a-3-b-road-transport-annex-hdv-files.zip/at_download/file"
 a <- tempdir()
-zipfile2016 <- paste0(a,"/1.A.3.b.i-iv Exhaust emissions from road transport annex HDV files.zip")
-download.file(url = link2016,destfile = zipfile2016)
-unzip(zipfile = zipfile2016, exdir = a)
-filepath2016 <- paste0(a,"/1.A.3.b.i-iv Exhaust emissions from road transport data annex Sept2014 update.xlsx")
-openxlsx::getSheetNames(filepath2016)
-#eq2016 <- openxlsx::read.xlsx(xlsxFile = filepath2016,sheet = "HDV - BUS Equations") %>% 
+zipfile2013 <- paste0(a,"/1.A.3.b.i-iv Exhaust emissions from road transport annex HDV files.zip")
+download.file(url = link2013,destfile = zipfile2013)
+unzip(zipfile = zipfile2013, exdir = a)
+filepath2013 <- paste0(a,"/1.A.3.b.i-iv Exhaust emissions from road transport data annex Sept2014 update.xlsx")
+openxlsx::getSheetNames(filepath2013)
+#eq2013 <- openxlsx::read.xlsx(xlsxFile = filepath2013,sheet = "HDV - BUS Equations") %>% 
 #  data.table::setDT()
-emep2016 <- openxlsx::read.xlsx(xlsxFile = filepath2016,sheet = "HDVs - BUS") %>% 
+emep2013 <- openxlsx::read.xlsx(xlsxFile = filepath2013,sheet = "HDVs - BUS") %>% 
   data.table::setDT()
 
 # organize 
@@ -70,29 +70,29 @@ emep2016 <- openxlsx::read.xlsx(xlsxFile = filepath2016,sheet = "HDVs - BUS") %>
 # 11               86 -9.713108e-05 0.02241283 2.552201 -1.785515 0.00121752 0.01119629
 # Hta Reduction.Factor.[%]                                   Function      Equation
 # 0.1641829                    0 ((a*x^2+b*x+c+d/x)/(e*x^2+f*x+g)*(1-rf))*k Equation 2019
-emep2016 <- emep2016[Sector %in% "Buses" & 
+emep2013 <- emep2013[Sector %in% "Buses" & 
                        `Sub-Sector` %like% "Bus" &
                        Pollutant %in% "FC",]
-emep2016[Technology %like% "SCR", Technology := "SCR"]
-emep2016[Technology %like% "EGR", Technology := "EGR"]
-emep2016[Technology %nlike% "EGR" & Technology %nlike% "SCR", Technology := NA]
-emep2016[,`:=`(`50` = NULL,
+emep2013[Technology %like% "SCR", Technology := "SCR"]
+emep2013[Technology %like% "EGR", Technology := "EGR"]
+emep2013[Technology %nlike% "EGR" & Technology %nlike% "SCR", Technology := NA]
+emep2013[,`:=`(`50` = NULL,
                R2 = NULL,
                EF = NULL,
                RF = 0,
                Fuel = "Diesel", 
                Mode = NA,
                k = 1)]
-emep2016[Euro.No. == 5, Euro.Standard := "Euro V"]
-emep2016[Euro.No. == 6, Euro.Standard := "Euro VI"]
-emep2016[,Euro.No. := NULL]
-emep2016[,Load := Load / 100]
-emep2016[is.na(Load), Load := 0.5]
-data.table::setnames(emep2016,"Slope","Road.Slope")
-emep2016[is.na(Road.Slope), Road.Slope := 0.0]
+emep2013[Euro.No. == 5, Euro.Standard := "Euro V"]
+emep2013[Euro.No. == 6, Euro.Standard := "Euro VI"]
+emep2013[,Euro.No. := NULL]
+emep2013[,Load := Load / 100]
+emep2013[is.na(Load), Load := 0.5]
+data.table::setnames(emep2013,"Slope","Road.Slope")
+emep2013[is.na(Road.Slope), Road.Slope := 0.0]
 # fix formula names
 # manual edit using formula below
-# emep2016[,.SD[1],by = .(Formula)][,.(Equation,Formula)] %>% dput()
+# emep2013[,.SD[1],by = .(Formula)][,.(Equation,Formula)] %>% dput()
 formulas_dt <- data.table::data.table(
   Equation = c("Equation 3", "Equation 12", "Equation 9", 
                "Equation 11", "Equation 4", "Equation 16", "Equation 10", "Equation 1"), 
@@ -104,20 +104,20 @@ formulas_dt <- data.table::data.table(
               "(exp((Alpha+(Beta/Speed))+(Gamma*log(Speed))))", 
               "(1/(Alpha+(Beta*Speed)))", 
               "(Alpha*(Speed^3)+(Beta*(Speed^2))+(Gamma*Speed)+Delta)"))
-  emep2016[formulas_dt,on = "Equation", Formula := i.Formula]
+  emep2013[formulas_dt,on = "Equation", Formula := i.Formula]
   # rename files, so it matches with 2019
-  head(emep2016,1)
-  dim(emep2016)
+  head(emep2013,1)
+  dim(emep2013)
   head(emep2019,1)
   dim(emep2019)
-  data.table::setnames(emep2016,old = c("Sector","Sub-Sector",
+  data.table::setnames(emep2013,old = c("Sector","Sub-Sector",
                                         "Vmin","Vmax","Equation","Formula","Ita"),
                        new = c("Category","Segment",
                                "Min.Speed.[km/h]","Max.Speed.[km/h]","Function_ID","Function","Hta"))
   
-  # reorder columns of 'emep2016' based on 'emep2019'
-  data.table::setcolorder(emep2016,neworder = names(emep2019))
-  head(emep2016,1)
+  # reorder columns of 'emep2013' based on 'emep2019'
+  data.table::setcolorder(emep2013,neworder = names(emep2019))
+  head(emep2013,1)
   head(emep2019,1)
   
   # add co2
@@ -129,11 +129,11 @@ formulas_dt <- data.table::data.table(
   # r_hc = 1.86, r_oc = 0.0
   # Eco2 = 44.011 * FC / (12.001 * r_hc + 16 * r_oc)
   #
-  fileco2 <- data.table::copy(emep2016)[,Pollutant := "CO2"]
+  fileco2 <- data.table::copy(emep2013)[,Pollutant := "CO2"]
   fileco2[,k := 44.011 / (12.001 * 1.86 + 16*0 ) ]
-  # rbind2016
+  # rbind2013
   
-  emep2016 <- data.table::rbindlist(list(emep2016,fileco2))
+  emep2013 <- data.table::rbindlist(list(emep2013,fileco2))
   
   # ---
   # EMEP 2009
@@ -179,7 +179,7 @@ formulas_dt <- data.table::data.table(
   for(i in list_euro){ emep2009[TEC_NAME %like% paste0(i," "), Euro.Standard := i]}
   emep2009[TEC_NAME %in% "Conventional", Euro.Standard := "Conventional"]
   # remove euro standard of phase "V" and "VI"
-  # since it has already been used in newer EF database (2016)
+  # since it has already been used in newer EF database (2013)
   emep2009 <- emep2009[Euro.Standard %nin% c("Euro V","Euro VI"),]
   emep2009[,`:=`(TEC_NAME = NULL, Mode = NA,
                  R2 = NULL, Subsegment = NULL, k =1,
@@ -212,14 +212,14 @@ formulas_dt <- data.table::data.table(
   #
   fileco2 <- data.table::copy(emep2009)[,Pollutant := "CO2"]
   fileco2[,k := 44.011 / (12.001 * 1.86 + 16*0 ) ]
-  # rbind2016
+  # rbind2013
   
   emep2009 <- data.table::rbindlist(list(emep2009,fileco2))
   
   #
   # rbindlist all emission factors
   #
-  europe <- data.table::rbindlist(list(emep2009,emep2016,emep2019))
+  europe <- data.table::rbindlist(list(emep2009,emep2013,emep2019))
   #
   # rename Function_ID
   europe[,Function_ID := .GRP, by = Function]
