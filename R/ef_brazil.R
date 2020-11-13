27638bd26aef9a6329a8bc847be802e2905d77c8
Joao Bazzo <joao.bazzo@gmail.com>
2020-11-13 00:16:43 -0300

    emission factors

--- R/ef_brazil.R
+++ R/ef_brazil.R
@@ -12,13 +12,11 @@
 #'  Evaporative emissions at average temperature ranges: "D_20_35", "S_20_35", "R_20_35", 
 #'  "D_10_25", "S_10_25", "R_10_25", "D_0_15", "S_0_15" and "R_0_15" where D means diurnal (g/day),
 #'   S hot/warm soak (g/trip) and R hot/warm running losses (g/trip).
-#' @param veh_type character; Vehicle categories by fuel: "PC_G", "PC_FG", "PC_FE", "PC_E", "LCV_G",
-#'  "LCV_FG", "LCV_FE", "LCV_E", "LCV_D", "TRUCKS_SL", "TRUCKS_L", "TRUCKS_M", "TRUCKS_SH",
-#'  "TRUCKS_H", "BUS_URBAN", "BUS_MICRO", "BUS_COACH", "BUS_ARTIC", "MC_G_150", "MC_G_150_500",
-#'  "MC_G_500", "MC_FG_150", "MC_FG_150_500", "MC_FG_500", "MC_FE_150", "MC_FE_150_500",
-#'  "MC_FE_500", "CICLOMOTOR", "GNV".
+#' @param veh_type character; Vehicle categories by fuel:"BUS_URBAN", "BUS_MICRO", "BUS_COACH", 
+#' "BUS_ARTIC", "MC_G_150", "MC_G_150_500".
 #' @param years numeric; Filter the emission factor to start from a specific base year. 
 #' If project is 'constant' values above 2017 and below 1980 will be repeated.
+#' @param as_list logical; Returns emission factors as a list, instead of data.table format. Default is TRUE.
 #' @return data.table; Emission factors in units 'g/km' by speed and model_year.
 #' @note 
 #' 
@@ -26,33 +24,10 @@
 #' 
 #' | vehicle       | description                                                    |
 #' | ------------- | --------------------------------------------------------------------- |
-#' | PC_G          | Passenger Car Gasohol (Gasoline + 27perc of anhydrous ethanol) |
-#' | PC_E	         | Passenger Car Ethanol (hydrous ethanol) |
-#' | PC_FG	       | Passenger Car Flex Gasohol (Gasoline + 27perc of anhydrous ethanol) |
-#' | PC_FE	       | Passenger Car Flex Ethanol (hydrous ethanol) |
-#' | LCV_G      	 | Light Commercial Vehicle Gasohol (Gasoline + 27perc of anhydrous ethanol) |
-#' | LCV_E      	 | Light Commercial Vehicle Ethanol (hydrous ethanol) |
-#' | LCV_FG	       | Light Commercial Vehicle Flex Gasohol (Gasoline + 27perc of anhydrous ethanol) |
-#' | LCV_FE	       | Light Commercial Vehicle Flex Ethanol (hydrous ethanol) |
-#' | LCV_D	       | Light Commercial Vehicle Diesel (5perc bio-diesel) |
-#' | TRUCKS_SL_D   | Trucks Semi Light Diesel (5perc bio-diesel) |
-#' | TRUCKS_L_D	   | Trucks Light Diesel (5perc bio-diesel) |
-#' | TRUCKS_M_D	   | Trucks Medium Diesel (5perc bio-diesel) |
-#' | TRUCKS_SH_D   | Trucks Semi Heavy Diesel (5perc bio-diesel) |
-#' | TRUCKS_H_D	   | Trucks Heavy Diesel (5perc bio-diesel) |
 #' | BUS_URBAN_D   | Urban Bus Diesel (5perc bio-diesel) |
 #' | BUS_MICRO_D   | Micro Urban Bus Diesel (5perc bio-diesel) |
 #' | BUS_COACH_D   | Coach (inter-state) Bus Diesel (5perc bio-diesel) |
 #' | BUS_ARTIC_D   | Articulated Urban Bus Diesel (5perc bio-diesel) |
-#' | MC_150_G	     | Motorcycle engine less than 150cc Gasohol (Gasoline + 27perc of anhydrous ethanol) |
-#' | MC_150_500_G	 | Motorcycle engine 150-500cc Gasohol (Gasoline + 27perc of anhydrous ethanol) |
-#' | MC_500_G	     | Motorcycle greater than 500cc Gasohol (Gasoline + 27perc of anhydrous ethanol) |
-#' | MC_150_FG	   | Flex Motorcycle engine less than 150cc Gasohol (Gasoline + 27perc of anhydrous ethanol) |
-#' | MC_150_500_FG | Flex Motorcycle engine 150-500cc Gasohol (Gasoline + 27perc of anhydrous ethanol) |
-#' | MC_500_FG	   | Flex Motorcycle greater than 500cc Gasohol (Gasoline + 27perc of anhydrous ethanol) |
-#' | MC_150_FE	   | Flex Motorcycle engine less than 150cc Ethanol (hydrous ethanol) |
-#' | MC_150_500_FE | Flex Motorcycle engine 150-500cc Ethanol (hydrous ethanol) |
-#' | MC_500_FE	   | Flex Motorcycle greater than 500cc Ethanol (hydrous ethanol) |
 #' 
 #' The percentage varies of biofuels varies by law.
 #' 
@@ -63,35 +38,20 @@
 #' 2) Also, the emission factors were extended till 50 years of use, repeating the oldest value.
 #' 3) CNG emission factors were expanded to other pollutants by comparison of US.EPA-AP42 emission
 #' factor: Section 1.4 Natural Gas Combustion.
-#' 
-#' In the previous versions I used the letter 'd' for deteriorated. I removed the letter 'd'
-#' internally to not break older code.
-#' If by mistake, the user inputs one of veh names from the old convention, they are internally
-#' changed to the new convention: "SLT", "LT", "MT", "SHT","HT", "UB", "SUB", "COACH", "ARTIC",
-#' "M_G_150", "M_G_150_500", "M_G_500", "M_FG_150", "M_FG_150_500", "M_FG_500", "M_FE_150",
-#' "M_FE_150_500","M_FE_500",
-#' If pollutant is "SO2", it needs 'sppm.' It is designed when 'veh' has length 1, if it has length
-#' 2 or more, it will show a warning
-#' Emission factor for vehicles older than the reported by CETESB were filled as the moving average
-#' of 2:
-#' Range EF from PC and LCV otto: 2018 - 1982. EF for 1981 and older as moving average.
 #' Range LCV diesel : 2018 - 2006. EF for 2005 and older as moving average.
-#' Range Trucks and Buses: 2018 - 1998. EF for 1997 and older as moving average.
-#' Range MC Gasoline: 2018 - 2003. EF for 2002 and older as moving average.
-#' Range MC Flex 150-500cc and >500cc: 2018 - 2012. EF for 2011 and older as moving average.
-#' Currently, 2020, there are not any system for recovery of fuel vapors in Brazil. 
-#' Hence, the FS takes into account the vapour that comes from the fuel tank inside the car and
-#' released into the atmosphere when injecting new fuel. There are discussions about incrementing
-#' implementing stage I and II and/or ORVR thesedays. The ef FS is calculated by transforming
-#' g FC/km into (L/KM)*g/L with g/L 1.14 fgor gasoline and 0.37 for ethanol (CETESB, 2016). The
-#' density considered is 0.75425 for gasoline and 0.809 for ethanol (t/m^3).
 #' @md
 #' @source \url{https://cetesb.sp.gov.br/veicular/relatorios-e-publicacoes/}
 #' @export
-ef_brazil <- function(pollutant, veh_type, years){
+#' @examples 
+#' ef_brazil(pollutant = c("CO2","NOx"),veh_type = "BUS_URBAN_D", years = 2016)
+ef_brazil <- function(pollutant, veh_type, years, as_list = TRUE){
+  
+  #
+  # init config
+  #
   # pollutant = c("CO","PM")
-  # veh_type = "BUS_URBAN_D" #veh_type
-  # years = total_fleet$year
+  # veh_type = c("BUS_MICRO_D","BUS_URBAN_D") #veh_type
+  # years = c(2001,2005)
   
   # check lengths----
   
@@ -113,10 +73,21 @@ ef_brazil <- function(pollutant, veh_type, years){
     return(ef_temp)
   })
 
-  ef_temp2 <- do.call(cbind, ef_temp1)  %>% units::set_units("g/km")  
+  ef_final <- do.call(cbind, ef_temp1)  %>% units::set_units("g/km")  
   
   # rename colnames
-  colnames(ef_temp2) <- paste0(rep(pollutant, each = length(years)), "_", years)
+  colnames(ef_final) <- paste0(rep(pollutant, each = length(years)), "_", years)
   
-  return(ef_temp2)
+  # return in a data.table/list like format----
+  
+  if(as_list){
+    # local test
+    ef_final <- list("pollutant" = rep(pollutant,each = length(veh_type)),
+                     "veh_type" = rep(veh_type,length(pollutant)),
+                     "years" = rep(years,length(pollutant)),
+                     "EF" = ef_final)
+  }
+  
+  return(ef_final)
+  
 }
--- R/ef_europe.R
+++ R/ef_europe.R
@@ -1,7 +1,7 @@
 #' @title Emission factor dependent on speed by EMEP/EEA
 #' 
-#' @description Returns a vector or data.frame of emission factors for buses based on EMEP/EEA.
-#' Function based on values from [EMEP/EEA air pollutant emission inventory guidebook 2019](https://www.eea.europa.eu/themes/air/air-pollution-sources-1/emep-eea-air-pollutant-emission-inventory-guidebook).
+#' @description Returns a list or data.frame of emission factors for buses based on EMEP/EEA.
+#' Function is based on values from [EMEP/EEA air pollutant emission inventory guidebook 2019](https://www.eea.europa.eu/themes/air/air-pollution-sources-1/emep-eea-air-pollutant-emission-inventory-guidebook).
 #' Estimates expressed in units 'g/km'.
 #' 
 #' @param speed units; Speed in 'km/h'.
@@ -11,32 +11,52 @@
 #' "Euro III", "IV", "V", "VI", and "EEV".
 #' @param pollutant character; Pollutant, classified in "CO", "NOx", "VOC", "PM", "FC" (Fuel
 #' Consumption), "CO2", "CH4", "NH3" and "N2O".
-# @param aggregate Logical; does the emission factor should be aggregated? Default is TRUE.
-# @param veh_distribution Numeric; Distribution of vehicle type, required only when 
-# aggregate == TRUE.
 #' @param fuel character; Fuel type, classified in "Diesel", "CNG", and "Biodiesel". Default is 
 #' "Diesel".
 #' @param tech character; Technology, classified in "SCR", "EGR", and "DPF+SCR" (for Euro IV). Default is "SCR".
 #' @param slope numeric; Slope gradient, classified in -0.06, -0.04, -0.02, 0.00, 0.02, 0.04 and 
-#' 0.06.
-#'  Negative gradients means downhills and positive uphills. Default is 0.0.
+#' 0.06. Negative gradients means downhills and positive uphills. Default is 0.0.
 #' @param load numeric; Load ratio, classified in 0.0, 0.5 and 1.0. Default is 0.5.
 #' @param fcorr numeric; Correction based on fuel composition. The length must be one per
 #' each euro standards. Default is 1.0.
-#' @return emission factors in units 'g/km' (a vector or a data.frame).
+#' @param as_list logical; Returns emission factors as a list, instead of data.table format. Default is TRUE.
+#' @return emission factors in units 'g/km' (list or a data.table).
 #' @export
+#' @examples 
+#' set.seed(1335)
+#' dist = units::set_units(rnorm(100,0.250,0.03),"km")
+#' ef_europe(speed = units::set_units(rnorm(100,50,5),"km/h"),
+#'                 veh_type = c("Urban Buses Standard 15 - 18 t","Urban Buses Articulated >18 t"),
+#'                 euro = c("IV","V"),
+#'                 pollutant = c("CO2","NOx"),
+#'                 fuel = "Diesel" ,
+#'                 tech =  c("SCR","EGR"),
+#'                 slope = 0.0,
+#'                 load = 0.5,
+#'                 fcorr = 1,
+#'                 as_list = TRUE)
 ef_europe <- function(speed, veh_type,  euro,  pollutant, fuel = "Diesel", tech = "SCR", 
-                      slope = 0.0, load = 0.5, fcorr = 1){
+                      slope = 0.0, load = 0.5, fcorr = 1,as_list = TRUE){
+  #
   # local test
-  # speed <- vein::Speed(1:100)
-  # veh_type <- "Urban Buses Standard 15 - 18 t"
-  # euro <- c("IV","V")
+  #
+  
+  # speed = rnorm(n = 100,mean = 50,sd = 5) %>% units::set_units("km/h")
+  # #speed = units::set_units(rep(1:100,2),"km/h")
+  # veh_type <- c("Urban Buses Standard 15 - 18 t","Urban Buses Articulated >18 t")
+  # euro <-c("IV","V")
   # pollutant <- c("CO2","NOx")
-  # fuel <- "Diesel";tech <- c("SCR","EGR"); slope = 0.0; load = 0.5; k =1; fcorr = 1;i = j = 1
-
+  # fuel <- "Diesel"
+  # tech <- c("SCR","EGR")
+  # slope = 0.0
+  # load = 0.5
+  # k =1
+  # fcorr = 1
+  # i = j = 1
+  
   # euro vector----
   
-  euro <- paste0("Euro ", euro)
+  neweuro <- paste0("Euro ", euro)
   temp_ef <- gtfs2emis::europe
   
   # check units and lengths----
@@ -48,33 +68,36 @@ ef_europe <- function(speed, veh_type,  euro,  pollutant, fuel = "Diesel", tech 
     stop("speed need to has 'units' in 'km/h'.")
   }
   
+  # speed----
+  
   speed <- round(as.numeric(speed))
-  speed[speed < 1] <- 1
-  maxSpeed <- max(speed)
+  tmpSpeed <- unique(speed)
+  tmpSpeed[tmpSpeed < 1] <- 1
   
-  # check lengths
+  # check lengths----
   
-  if(length(euro) != length(veh_type) && length(veh_type) == 1){
-    veh_type <- rep(veh_type,length(euro))
+  if(length(neweuro) != length(veh_type) && length(veh_type) == 1){
+    veh_type <- rep(veh_type,length(neweuro))
   }
-  if(length(euro) != length(tech) && length(tech) == 1){
-    tech <- rep(tech,length(euro))
+  if(length(neweuro) != length(tech) && length(tech) == 1){
+    tech <- rep(tech,length(neweuro))
   }
-  if(length(euro) != length(fuel) && length(fuel) == 1){
-    fuel <- rep(fuel,length(euro))
+  if(length(neweuro) != length(fuel) && length(fuel) == 1){
+    fuel <- rep(fuel,length(neweuro))
   }
   if(length(fcorr) == 1){
-    fcorr <- rep(fcorr,length(euro))
+    fcorr <- rep(fcorr,length(neweuro))
   }
   if(length(slope) == 1){
-    slope <- rep(slope,length(euro))
+    slope <- rep(slope,length(speed))
   }
   if(length(load) == 1){
-    load <- rep(load,length(euro))
+    load <- rep(load,length(speed))
   }
   if(length(fcorr) == 1){
-    fcorr <- rep(fcorr,length(euro))
+    fcorr <- rep(fcorr,length(neweuro))
   }
+  
   # polynomial expression----
   
   eq_num <- function(Alpha,Beta,Gamma,Delta,Epsilon,Zita,Hta,RF,Speed,Function_ID,k,fcorr){
@@ -94,63 +117,110 @@ ef_europe <- function(speed, veh_type,  euro,  pollutant, fuel = "Diesel", tech 
   }
   
   # emission factor----
-  temp_ef1 <- lapply(seq_along(pollutant),function(j){  # j = 1
-    temp_ef2 <- lapply(seq_along(euro),function(i){    # i = 2
+  
+  temp_ef1 <- lapply(seq_along(pollutant),function(i){  # i = 1
+    temp_ef2 <- lapply(seq_along(neweuro),function(j){    # j = 1
       
-      # pre-conditions
-      #message(paste0(pollutant[j]))
-      #message(paste0(euro[i]))
-      #message(i)
-      if(euro[i] %in%  c("Conventional","Euro I","Euro II","Euro III")){
-        tech[i] = NA
-        message(paste0("no technology associated with ", euro[i]))
+      #
+      # condition for missing technologies
+      #
+      
+      if(neweuro[j] %in%  c("Conventional","Euro I","Euro II","Euro III")){
+        tech[j] = NA
+        message(paste0("no technology associated with ", neweuro[j]))
       }
-      temp_ef3 <- temp_ef[ Pollutant %in% pollutant[j] &  
-                             Fuel %in% fuel[i] & 
-                             Segment %in% veh_type[i] & 
-                             Technology %in% tech[i] &
-                             Euro.Standard %in% euro[i] & 
-                             Road.Slope %in% slope[i] & 
-                             Load == load[i], ]
       
-      # no EF
+      #
+      # fix load and slope
+      #
+      
+      if(length(unique(slope)) > 1 & length(unique(slope)) > 1){
+        temp_ef3 <- lapply(seq_along(neweuro),function(k){ # k = 1
+          temp_ef[ Pollutant %in% pollutant[i] &  
+                     Fuel %in% fuel[j] & 
+                     Segment %in% veh_type[j] & 
+                     Technology %in% tech[j] &
+                     Euro.Standard %in% neweuro[j] & 
+                     Road.Slope %in% slope[k] & 
+                     Load == load[k], ]
+        }) %>% data.table::rbindlist()
+      }else{
+        temp_ef3 <- temp_ef[ Pollutant %in% pollutant[i] &  
+                               Fuel %in% fuel[j] & 
+                               Segment %in% veh_type[j] & 
+                               Technology %in% tech[j] &
+                               Euro.Standard %in% neweuro[j] & 
+                               Road.Slope %in% slope[1] & 
+                               Load == load[1], ]
+      }
+      
+      #
+      # no EF case
+      #
+      
       if(nrow(temp_ef3) == 0){
         erro_msg <- paste("No available emission factor for the following combination of parameters\n",
                           "fuel = ",fuel[i],"| veh_type = ",veh_type[i],
-                          "\n euro = ",euro[i],"| tech = ",tech[i],
-                          "| pollutant = ",pollutant[j],"| slope = ",slope[i],
+                          "\n euro = ",neweuro[i],"| tech = ",tech[i],
+                          "| pollutant = ",pollutant[i],"| slope = ",slope[i],
                           "| load = ",load[i],
                           "\n Please check `data(europe)` for available data.")
         stop(erro_msg)
       }
       
-      
+      #
       # fix speed
-      tmpSpeed <- 1:maxSpeed
+      #
       
-      tmpSpeed[tmpSpeed < temp_ef3$`Min.Speed.[km/h]`] <- temp_ef3$`Min.Speed.[km/h]`
-      tmpSpeed[tmpSpeed > temp_ef3$`Max.Speed.[km/h]`] <- temp_ef3$`Max.Speed.[km/h]`
+      tmpSpeedPol <- tmpSpeed
+      tmpSpeedPol[tmpSpeedPol < temp_ef3$`Min.Speed.[km/h]`] <- temp_ef3$`Min.Speed.[km/h]`
+      tmpSpeedPol[tmpSpeedPol > temp_ef3$`Max.Speed.[km/h]`] <- temp_ef3$`Max.Speed.[km/h]`
       
-      # return
+      #
+      # Ef estimates
+      #
       
       eq_output <- eq_num(Alpha = temp_ef3$Alpha, Beta = temp_ef3$Beta, 
                           Gamma = temp_ef3$Gamma, Delta = temp_ef3$Delta, 
                           Epsilon = temp_ef3$Epsilon, Zita = temp_ef3$Zita,
                           Hta = temp_ef3$Hta, RF = temp_ef3$RF,
-                          Speed = tmpSpeed, Function_ID = temp_ef3$Function_ID, 
+                          Speed = tmpSpeedPol, Function_ID = temp_ef3$Function_ID, 
                           k = temp_ef3$k, fcorr = fcorr[i])
+      
+      # expands speed---
+      
+      data_speed <- data.table::data.table("ef" = eq_output, "tmpSpeed" = tmpSpeed)
+      data.table::setkey(data_speed,tmpSpeed)
+      eq_output <- data_speed[data.table::data.table(speed)]$ef
+      
       return(eq_output)
     })
     
-    # do not aggregate emission factors
+    #
+    # do not aggregate EF
+    #
+    
     temp_ef3 <- do.call(cbind, temp_ef2) %>% 
       units::set_units('g/km') %>% data.table::as.data.table()
-    names(temp_ef3) <- paste0(pollutant[j], "_", rep(euro))
+    names(temp_ef3) <- paste0(pollutant[i],"_Euro_", rep(euro))
     return(temp_ef3)
   })
   
-  # return in a data.table like format----
+  # return in a data.table/list like format----
   
   ef_final <- do.call(cbind, temp_ef1)
-  return(ef_final[speed, ])
+  
+  if(as_list){
+    # local test
+    ef_final <- list("pollutant" = rep(pollutant,each = length(veh_type)),
+                     "veh_type" = rep(veh_type,length(pollutant)),
+                     "euro" = rep(neweuro,length(pollutant)),
+                     "fuel" = rep(fuel,length(pollutant)),
+                     "tech" = rep(tech,length(pollutant)),
+                     "slope" = slope,
+                     "load" = load,
+                     "EF" = ef_final)
+  }
+  
+  return(ef_final)
 }
