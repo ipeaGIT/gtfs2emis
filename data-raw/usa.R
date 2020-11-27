7442fa69b2d33b65c2a5b0c6820e8489832be2fe
Joao Bazzo <joao.bazzo@gmail.com>
2020-11-27 17:29:57 -0300

    add list option in ef_usa

--- data-raw/usa.R
+++ data-raw/usa.R
@@ -1,9 +1,27 @@
+#
+# prep EF from EMFAC (USA)
+#
+
 rm(list=ls())
-#devtools::load_all()
 library(data.table)
 library(magrittr)
+library(vein)
 `%nin%` = Negate(`%in%`)
+
+
 emfac <- data.table::fread("../../Dropbox/IPEA/data-raw/emission_factors/EMFAC/EMFAC2017-ER-2011Class-Statewide2010-2011-2012-2013-2014-2015-2016-2017-2019-2020-Annual-20200512000741.csv")
+
+
+#
+# generate fuel correction
+#
+
+source("data-raw/usa_fc.R")
+scf <- scf_emfac()
+scf[,speed := round(speed,0)]
+#
+#
+#
 #emfac <- data.table::fread("../../../joaobazzo/Downloads/EMFAC2017-EI-2011Class-Statewide2010-2011-2012-2013-2014-2015-2016-2017-2019-2020-Annual-20201112220725.csv")
 #emfac <- data.table::fread("../../Dropbox/IPEA/data-raw/emission_factors/EMFAC/EMFAC2017-ER-2011Class-Statewide2010-2011-2012-2013-2014-2015-2016-2017-2019-2020-Annual-20201116093432.csv")
 #emfac
@@ -46,7 +64,53 @@ emfac02[,EF_1s := data.table::frollmean(EF,n = 2), by = .(`Calendar Year`,
                                                           Fuel,Pollutant)]
 emfac02[is.na(EF_1s),EF_1s := EF]
 
+# add scf into emission factor
+
+emfac03 <- emfac02[scf, on = c("Pollutant" = "pollutant",
+                    "speed" = "speed",
+                    "Fuel" = "fuel",
+                    "Model Year" = "year")]
+emfac03 <- emfac03[complete.cases(emfac03)]
+emfac03 <- emfac03[,cor_ef := EF_1s * scf]
+emfac03
 break()
+library(ggplot2)
+p = "CO2"
+ef_br <- vein::ef_cetesb(p = p,veh = "BUS_URBAN_D",year = 2014,agemax = 1)
+ef_sc_br <- vein::ef_hdv_scaled(dfcol = ef_br,v = "Ubus",t = "Std",g = ">15 & <=18",
+                                eu = "IV",p = p,gr = 0.00,l = 0.5) 
+tmpSpeed <- 1:65 %>% units::set_units("mile/h") %>% units::set_units("km/h") %>% as.numeric()
+ef_sc_br <- data.table::data.table("speed" = 1:65,"ef" = ef_sc_br[[1]](tmpSpeed))
+
+
+ggplot() + 
+  geom_line(data = emfac03[Pollutant %in% p & Fuel %in% "Diesel" & 
+                             `Model Year` == 2015 & `Calendar Year` == 2014,],
+            aes(x = speed,y = EF_1s))+
+  geom_point(data = emfac03[Pollutant %in% p & Fuel %in% "Diesel" & 
+                              `Model Year` == 2015 & `Calendar Year` == 2014,],
+             aes(x = speed,y = cor_ef),color = "red")+
+  geom_hline(yintercept = as.numeric(ef_br),col = "darkgreen") +
+  geom_line(data = ef_sc_br,aes(x = speed, y = ef_sc_br$ef),col="purple") +
+  labs(x = "speed (miles/h)", y= "Ef (g/km)",title = p)
+
+
+
+geom_point(data = emfac03[Pollutant %in% "CO" & Fuel %in% "Diesel" & 
+                            `Model Year` == 2015 & `Calendar Year` == 2014,],
+           aes(x = speed,y = cor_ef),color = "red")
+# 
+# link_vein <- "https://github.com/atmoschem/vein/blob/master/R/sysdata.rda?raw=true"
+# download.file(url = link_vein,destfile = "sysdata.rda")
+load("sysdata.rda")
+library(data.table)
+emfac_agg <- sysdata$emfac_agg
+colPol <- colnames(emfac_agg)[colnames(emfac_agg) %like% paste0("CO2","_RUNEX")]
+emfac_agg[Vehicle_Category %like% "UBUS" & Fuel %in% "Diesel" & 
+  `Model_Year` == 2015 & `Calendar_Year` == 2020,]
+library(vein)
+vein::ef
+vein::ef_emfac(veh = "UBUS", fuel = "Diesel",pol = "CO2_RUNEX")
 emfac02 %>% head(10)
 emfac02[,.SD[1],by = .(`Calendar Year`,
                       `Model Year`,
