ce72be6bccb6ab129661029a1c51e4d29256bf74
Joao Bazzo <joao.bazzo@gmail.com>
2021-07-22 12:39:15 -0300

    adjust EMFAC EF

--- R/ef_usa.R
+++ R/ef_usa.R
@@ -15,7 +15,7 @@
 #'   Nitrous Oxide (N2O), and Methane (CH4).
 #' @param calendar_year numeric; Calendar Year between 2010 - 2020. Year in which the emissions
 #' inventory is estimated, in order to consider the effect of degradation.
-#' @param fuel character; Type of fuel: 'Diesel','Gasoline','Natural Gas'. Default is 'Diesel'.
+#' @param fuel character; Type of fuel: 'D' (Diesel),'G' (Gasoline),'CNG' (Compressed Natural Gas). Default is 'D'.
 #' @param model_year numeric; Model year of vehicle.
 #' @param speed units; Speed in 'km/h'; Emission factor are returned in speed intervals 
 #'  such as "5-10", "10-15", "15-20", "20-25", "25-30", "30-35", "35-40", "40-45", "45-50",
@@ -34,20 +34,21 @@
 #'         calendar_year = 2019,
 #'         model_year = 2015,
 #'         speed = units::set_units(1:100,"km/h"),
-#'         fuel = "Diesel",
+#'         fuel = "D",
 #'         as_list = TRUE)
 #'         
 ef_usa <- function(pollutant, calendar_year, model_year, speed, fuel = 'Diesel', as_list = TRUE){
-  # pollutant = c("CO","PM10")
+  
+  # pollutant = c("CO","PM10","CH4")
   # calendar_year = "2019"
-  # model_year = "2014"
+  # model_year = c("2014","2013")
   # speed = units::set_units(1:100,"km/h")
-  # fuel = "Diesel"
+  # fuel = c("Diesel","CNG")
   
   # Filter calendar and fuel----
   
-  temp_emfac <- usa[`Calendar Year` %in% as.character(calendar_year) &
-                      Fuel %in% fuel, ]
+  temp_emfac <- usa[`Calendar Year` %in% as.character(unique(calendar_year)) &
+                      Fuel %in% unique(fuel), ]
   
   # check units and lengths----
   
@@ -59,62 +60,57 @@ ef_usa <- function(pollutant, calendar_year, model_year, speed, fuel = 'Diesel',
   }
   speed <- as.numeric(speed)
   
-  # Filter pollutant
+  # fuel
+  if(length(model_year) != length(fuel) && length(fuel) == 1){
+    fuel <- rep(fuel,length(model_year))
+  }
+  # model_year
+  if(length(model_year) != length(fuel) && length(model_year) == 1){
+    model_year <- rep(model_year,length(fuel))
+  }
   
-  ef_pol <- lapply(pollutant, function(p){     # p = pollutant[1]
-    temp_emfac <- temp_emfac[Pollutant %in% p, ]
-    
-    # check condition
-    
-    if(dim(temp_emfac)[1] == 0){
-      stop("Emission Factor do not exist. \n Please check `data(usa)` for valid emission factors.")
-    }
-    
+  # Filter pollutant
+  ef_pol <- lapply(pollutant, function(i){   # i = pollutant[1]
     # Filter Model Year----
-    
-    temp_ef <- sapply(model_year, function(i){       # i = model_year[1]
-      temp_emfac <- temp_emfac[`Model Year` %in% as.character(i), ]
-      
+    temp_ef0 <- sapply(seq_along(model_year), function(j){   # j = 1
+      # model_year
+      temp_emfac2 <- data.table::copy(temp_emfac)[Pollutant %in% i & 
+                                                    `Model Year` %in% as.character(model_year[j]) & 
+                                                    Fuel %in% fuel[j], ]
       # check condition (2)
-      if(dim(temp_emfac)[1] == 0){
+      if(dim(temp_emfac2)[1] == 0){
         stop("Emission Factor do not exist. \nPlease check `data(usa)` for valid emission factors.")
       }
-      
-      temp_model <- temp_emfac[, EF]
-      
-      return(temp_model)
-    }) %>% data.table::as.data.table()
-    
-    # rename columns
-    
-    names(temp_ef) <- paste0(p, "_", model_year)
-    return(temp_ef)
-  })
-  
-  # return in a data.table like format
-  
+      return(temp_emfac2[, EF])
+    }) 
+    temp_ef0 <- data.table::as.data.table(temp_ef0)
+    names(temp_ef0) <- paste(i, as.character(model_year),fuel,sep = "_")
+    return(temp_ef0)
+  }) 
+ef_pol
   ef_final <- do.call(cbind, ef_pol)
-  # Filter Speed based on emission factor data.table speed interval
-  temp_speed <- rep(NA, length(speed))
-  temp_order <- unique(usa$upper_speed_interval)
-  temp_order <- temp_order[order(temp_order, decreasing = TRUE)]
-  for(t in temp_order){ # t = temp_order[1]
-    temp_speed[which(speed < t)] <- t
-  }
-  #temp_speed
-  
-  ef_final <- ef_final[as.integer(as.factor(temp_speed)), ]
-  ef_final <- ef_final[, lapply(.SD, units::set_units, "g/km")]
-  
-  # list
-  if(as_list == TRUE){
-    # local test
-    ef_final <- list("pollutant" = rep(pollutant,each = length(model_year)),
-                     "model_year" = rep(model_year,length(pollutant)),
-                     "fuel" = rep(fuel,length(pollutant)),
-                     "EF" = ef_final)
-  }
-  
-  return(ef_final)
+
+# Filter Speed based on emission factor data.table speed interval
+temp_speed <- rep(NA, length(speed))
+temp_order <- unique(usa$upper_speed_interval)
+temp_order <- temp_order[order(temp_order, decreasing = TRUE)]
+for(t in temp_order){ # t = temp_order[1]
+  temp_speed[which(speed < t)] <- t
+}
+#temp_speed
+
+ef_final <- ef_final[as.integer(as.factor(temp_speed)), ]
+ef_final <- ef_final[, lapply(.SD, units::set_units, "g/km")]
+
+# list
+if(as_list == TRUE){
+  # local test
+  ef_final <- list("pollutant" = rep(pollutant,each = length(model_year)),
+                   "model_year" = rep(model_year,length(pollutant)),
+                   "fuel" = rep(fuel,length(pollutant)),
+                   "EF" = ef_final)
+}
+
+return(ef_final)
 }
 
--- data-raw/usa.R
+++ data-raw/usa.R
@@ -1,5 +1,5 @@
 #
-# prep EF from EMFAC (USA)
+# prep EF from EMFAC (USA)----
 #
 
 rm(list=ls())
@@ -9,151 +9,173 @@ library(vein)
 `%nin%` = Negate(`%in%`)
 
 
-emfac <- data.table::fread("../../Dropbox/IPEA/data-raw/emission_factors/EMFAC/EMFAC2017-ER-2011Class-Statewide2010-2011-2012-2013-2014-2015-2016-2017-2019-2020-Annual-20200512000741.csv")
+emfac <- data.table::fread("../../Dropbox/IPEA/gtfs2gps/data-raw/emission_factors/EMFAC/EMFAC2017-ER-2011Class-Statewide2010-2011-2012-2013-2014-2015-2016-2017-2019-2020-Annual-20200512000741.csv")
 
 
 #
 # generate fuel correction
 #
 
-source("data-raw/usa_fc.R")
-scf <- scf_emfac()
-scf[,speed := round(speed,0)]
+# source("data-raw/usa_fc.R")
+# scf <- scf_emfac()
+# scf[,speed := round(speed,0)]
 #
 #
 #
 #emfac <- data.table::fread("../../../joaobazzo/Downloads/EMFAC2017-EI-2011Class-Statewide2010-2011-2012-2013-2014-2015-2016-2017-2019-2020-Annual-20201112220725.csv")
 #emfac <- data.table::fread("../../Dropbox/IPEA/data-raw/emission_factors/EMFAC/EMFAC2017-ER-2011Class-Statewide2010-2011-2012-2013-2014-2015-2016-2017-2019-2020-Annual-20201116093432.csv")
-#emfac
+
+
+# fix categories
 emfac <- emfac[`Vehicle Category` %in% 'UBUS',]
+
+
+# 1) fix fuel names ----
+emfac$Fuel %>% unique()
 emfac[Fuel %in% "Natural Gas", Fuel := "CNG"]
+emfac[Fuel %in% "Diesel", Fuel := "D"]
+emfac[Fuel %in% "Gasoline", Fuel := "G"]
+
+# 2) units of VMT----
 emfac[,VMT := VMT %>% 
         units::set_units('mile') %>% 
         units::set_units("km")]
 
 
+# 3) rearrange data cols----
 colpol <- names(emfac)[names(emfac) %like% "RUNEX"]
-#colpol <- gsub("_RUNEX","",colpol)
-#emfac[,(colpol) := get(colpol) * VMT]
 emfac <- lapply(colpol,function(i){ # i = colpol[1]
   ef_pol <- emfac[,c('Calendar Year','Model Year','Speed','Fuel','VMT',i),with = FALSE]
   ef_pol[,Pollutant := gsub("_RUNEX","",i)]
   names(ef_pol) <- c(names(ef_pol)[1:5],"EF","Pollutant")
   return(ef_pol)
 }) %>% data.table::rbindlist()
-# speed intervals
+
+# 4) add speed intervals----
 emfac[,lower_speed_interval := Speed - 5]
 emfac[,upper_speed_interval := Speed]
 emfac[Speed %in% 90,upper_speed_interval := 130]
 emfac[,Speed := NULL]
 
-# stretch
+
+
+# 5) remove EF equal to zero----
+emfac <- emfac[EF > 0,]
+
+
+
+# 6) adjust to all speeds-----
 emfac01 <- lapply(1:nrow(emfac),function(i){ # i = 1
-  speed_tmp <- emfac[i,lower_speed_interval]:(emfac[i,upper_speed_interval]-1)
-  # update
-  tmp_dt <- data.table::data.table(emfac[i,],"speed" = speed_tmp)
-  # as.factor
-  #coltemp <- colnames(emfac[i,])
-  #tmp_dt[,(coltemp) := lapply(.SD,as.factor), .SDcols = coltemp]
+  tmp_dt <- data.table::data.table(emfac[i,]
+                                   ,"speed" = emfac[i,lower_speed_interval]:(emfac[i,upper_speed_interval]-1)
+                                   )
   return(tmp_dt)
 }) %>% data.table::rbindlist()
 
-emfac02 <- data.table::copy(emfac01)[EF > 0,]
+
+# 7) add a moving average----
+
+emfac02 <- data.table::copy(emfac01)
 emfac02[,EF_1s := data.table::frollmean(EF,n = 2), by = .(`Calendar Year`,
                                                           `Model Year`,
                                                           Fuel,Pollutant)]
 emfac02[is.na(EF_1s),EF_1s := EF]
+emfac02[,EF_1s := EF_1s %>% 
+          units::set_units("g/mile") %>% 
+          units::set_units("g/km")]
 
-# add scf into emission factor
+# 8) setnames
+usa <- data.table::copy(emfac02)
+usa[,EF := NULL]
 
-emfac03 <- emfac02[scf, on = c("Pollutant" = "pollutant",
-                    "speed" = "speed",
-                    "Fuel" = "fuel",
-                    "Model Year" = "year")]
-emfac03 <- emfac03[complete.cases(emfac03)]
-emfac03 <- emfac03[,cor_ef := EF_1s * scf]
-emfac03
-break()
-library(ggplot2)
-p = "CO2"
-ef_br <- vein::ef_cetesb(p = p,veh = "BUS_URBAN_D",year = 2014,agemax = 1)
-ef_sc_br <- vein::ef_hdv_scaled(dfcol = ef_br,v = "Ubus",t = "Std",g = ">15 & <=18",
-                                eu = "IV",p = p,gr = 0.00,l = 0.5) 
-tmpSpeed <- 1:65 %>% units::set_units("mile/h") %>% units::set_units("km/h") %>% as.numeric()
-ef_sc_br <- data.table::data.table("speed" = 1:65,"ef" = ef_sc_br[[1]](tmpSpeed))
-
-
-ggplot() + 
-  geom_line(data = emfac03[Pollutant %in% p & Fuel %in% "Diesel" & 
-                             `Model Year` == 2015 & `Calendar Year` == 2014,],
-            aes(x = speed,y = EF_1s))+
-  geom_point(data = emfac03[Pollutant %in% p & Fuel %in% "Diesel" & 
-                              `Model Year` == 2015 & `Calendar Year` == 2014,],
-             aes(x = speed,y = cor_ef),color = "red")+
-  geom_hline(yintercept = as.numeric(ef_br),col = "darkgreen") +
-  geom_line(data = ef_sc_br,aes(x = speed, y = ef_sc_br$ef),col="purple") +
-  labs(x = "speed (miles/h)", y= "Ef (g/km)",title = p)
-
-
-
-geom_point(data = emfac03[Pollutant %in% "CO" & Fuel %in% "Diesel" & 
-                            `Model Year` == 2015 & `Calendar Year` == 2014,],
-           aes(x = speed,y = cor_ef),color = "red")
-# 
-# link_vein <- "https://github.com/atmoschem/vein/blob/master/R/sysdata.rda?raw=true"
-# download.file(url = link_vein,destfile = "sysdata.rda")
-load("sysdata.rda")
-library(data.table)
-emfac_agg <- sysdata$emfac_agg
-colPol <- colnames(emfac_agg)[colnames(emfac_agg) %like% paste0("CO2","_RUNEX")]
-emfac_agg[Vehicle_Category %like% "UBUS" & Fuel %in% "Diesel" & 
-  `Model_Year` == 2015 & `Calendar_Year` == 2020,]
-library(vein)
-vein::ef
-vein::ef_emfac(veh = "UBUS", fuel = "Diesel",pol = "CO2_RUNEX")
-emfac02 %>% head(10)
-emfac02[,.SD[1],by = .(`Calendar Year`,
-                      `Model Year`,
-                      Fuel,Pollutant,
-                      speed)] %>% head(7)
-emfac02[1:15,]
-
-# units
-#emfac[,lower_speed_interval := lower_speed_interval %>% 
-#        units::set_units('mile/h') %>% 
-#        units::set_units('km/h')]
-#emfac[,upper_speed_interval := upper_speed_interval %>% 
-#        units::set_units('mile/h') %>% 
-#        units::set_units('km/h')]
-emfac[,EF := EF %>% 
-        units::set_units('g/mile') %>% 
-        units::set_units('g/km')]
-
-emfac <- emfac[as.numeric(EF) > 0,]
-#emfac[,EF_VMT := VMT * EF]
-emfac[,EF := units::set_units(EF,'g/mile')]
-
-data_speed <- data.table::data.table("speed" = 1:120)
-
-emfac01 <- emfac[data_speed,on = ("lower_speed_interval == speed")]
-
-emfac_speed <- emfac
-effsped <- data.table::copy(emfac_speed)[Pollutant %in% "CO2" & `Model Year` == '2002' &
-                                           Fuel == "Diesel" & `Calendar Year` == 2016, ] 
-effnos <- data.table::copy(emfac)[Pollutant %in% "CO2" & `Model Year` == '2002' & 
-                                    Fuel == "Diesel"  & `Calendar Year` == 2016, ] 
-
-emfac$Pollutant %>% unique()
-plot(x = effsped$lower_speed_interval,y = effsped$EF)
-effnos$EF %>% units::set_units("g/km")
-
-ef_brazil(pollutant = "CO2",veh_type = "BUS_URBAN_D",
-          years = 2007,as_list = FALSE) %>% units::set_units("g/km")
-
-# write
-# data.table::fwrite(emfac,"data/emfac.txt")
-usa <- emfac
+data.table::setnames(usa,"EF_1s","EF")
 usethis::use_data(usa,overwrite = TRUE)
+
+
+
+
+
+
+
+# # add scf into emission factor
+# 
+# emfac03 <- emfac02[scf, on = c("Pollutant" = "pollutant",
+#                                "speed" = "speed",
+#                                "Fuel" = "fuel",
+#                                "Model Year" = "year")]
+# emfac03 <- emfac03[complete.cases(emfac03)]
+# emfac03 <- emfac03[,cor_ef := EF_1s * scf]
+# emfac03
+# break()
+# library(ggplot2)
+# p = "CO2"
+# ef_br <- vein::ef_cetesb(p = p,veh = "BUS_URBAN_D",year = 2014,agemax = 1)
+# ef_sc_br <- vein::ef_hdv_scaled(dfcol = ef_br,v = "Ubus",t = "Std",g = ">15 & <=18",
+#                                 eu = "IV",p = p,gr = 0.00,l = 0.5) 
+# tmpSpeed <- 1:65 %>% units::set_units("mile/h") %>% units::set_units("km/h") %>% as.numeric()
+# ef_sc_br <- data.table::data.table("speed" = 1:65,"ef" = ef_sc_br[[1]](tmpSpeed))
+# 
+# 
+# ggplot() + 
+#   geom_line(data = emfac03[Pollutant %in% p & Fuel %in% "Diesel" & 
+#                              `Model Year` == 2015 & `Calendar Year` == 2014,],
+#             aes(x = speed,y = EF_1s))+
+#   geom_point(data = emfac03[Pollutant %in% p & Fuel %in% "Diesel" & 
+#                               `Model Year` == 2015 & `Calendar Year` == 2014,],
+#              aes(x = speed,y = cor_ef),color = "red")+
+#   geom_hline(yintercept = as.numeric(ef_br),col = "darkgreen") +
+#   geom_line(data = ef_sc_br,aes(x = speed, y = ef_sc_br$ef),col="purple") +
+#   labs(x = "speed (miles/h)", y= "Ef (g/km)",title = p)
+# 
+# 
+# 
+# geom_point(data = emfac03[Pollutant %in% "CO" & Fuel %in% "Diesel" & 
+#                             `Model Year` == 2015 & `Calendar Year` == 2014,],
+#            aes(x = speed,y = cor_ef),color = "red")
+# # 
+# # link_vein <- "https://github.com/atmoschem/vein/blob/master/R/sysdata.rda?raw=true"
+# # download.file(url = link_vein,destfile = "sysdata.rda")
+# load("sysdata.rda")
+# # library(data.table)
+# # emfac_agg <- sysdata$emfac_agg
+# # colPol <- colnames(emfac_agg)[colnames(emfac_agg) %like% paste0("CO2","_RUNEX")]
+# # emfac_agg[Vehicle_Category %like% "UBUS" & Fuel %in% "Diesel" & 
+# #             `Model_Year` == 2015 & `Calendar_Year` == 2020,]
+# # library(vein)
+# # vein::ef
+# # vein::ef_emfac(veh = "UBUS", fuel = "Diesel",pol = "CO2_RUNEX")
+# # emfac02 %>% head(10)
+# # emfac02[,.SD[1],by = .(`Calendar Year`,
+# #                        `Model Year`,
+# #                        Fuel,Pollutant,
+# #                        speed)] %>% head(7)
+# # emfac02[1:15,]
+# 
+# 
+# 
+# emfac <- emfac[as.numeric(EF) > 0,]
+# #emfac[,EF_VMT := VMT * EF]
+# emfac[,EF := units::set_units(EF,'g/mile')]
+# 
+# data_speed <- data.table::data.table("speed" = 1:120)
+# 
+# emfac01 <- emfac[data_speed,on = ("lower_speed_interval == speed")]
+# 
+# emfac_speed <- emfac
+# effsped <- data.table::copy(emfac_speed)[Pollutant %in% "CO2" & `Model Year` == '2002' &
+#                                            Fuel == "Diesel" & `Calendar Year` == 2016, ] 
+# effnos <- data.table::copy(emfac)[Pollutant %in% "CO2" & `Model Year` == '2002' & 
+#                                     Fuel == "Diesel"  & `Calendar Year` == 2016, ] 
+# 
+# emfac$Pollutant %>% unique()
+# plot(x = effsped$lower_speed_interval,y = effsped$EF)
+# effnos$EF %>% units::set_units("g/km")
+# 
+# ef_brazil(pollutant = "CO2",veh_type = "BUS_URBAN_D",
+#           years = 2007,as_list = FALSE) %>% units::set_units("g/km")
+# 
+# # write
+# data.table::fwrite(emfac,"data/emfac.txt")
 
 
 
