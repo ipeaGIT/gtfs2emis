ec6554ed6ea6860a7703b4ded7537ce6cabd2855
Joaobazzo <joao.bazzo@gmail.com>
2021-08-11 11:40:38 -0300

    add compatible speed intervals (close #53)

--- data-raw/usa.R
+++ data-raw/usa.R
@@ -9,7 +9,8 @@ library(vein)
 `%nin%` = Negate(`%in%`)
 
 
-emfac <- data.table::fread("../../Dropbox/IPEA/gtfs2gps/data-raw/emission_factors/EMFAC/EMFAC2017-ER-2011Class-Statewide2010-2011-2012-2013-2014-2015-2016-2017-2019-2020-Annual-20200512000741.csv")
+#emfac <- data.table::fread("../../Dropbox/IPEA/gtfs2gps/data-raw/emission_factors/EMFAC/EMFAC2017-ER-2011Class-Statewide2010-2011-2012-2013-2014-2015-2016-2017-2019-2020-Annual-20200512000741.csv")
+emfac <- data.table::fread("data-raw/EMFAC2017-ER-2011Class-Statewide2010-2011-2012-2013-2014-2015-2016-2017-2019-2020-Annual-20200512000741.csv")
 names(emfac) <- janitor::make_clean_names(names(emfac))
 
 #
@@ -87,6 +88,66 @@ emfac[,ef := ef %>%
 
 emfac[as.numeric(lower_speed_interval) > 0
       , lower_speed_interval := lower_speed_interval + units::set_units(1,"km/h")]
+
+
+# check and adjust size of speed bins------
+
+# The final data has keep speed bins intervals (SBI) with total size of 14.
+# However, there are cases where only 11 or 13 SBI are available.
+# In these cases, we need to fulfill the last two or three maximum SBI
+# which are 
+# 90 - 97 km/h,
+# 98 - 105 km/h and
+# 106 - 113 km/h.
+
+# check cases with SBI < 14
+tmp_emfac <- data.table::copy(emfac)
+tmp_emfac <- tmp_emfac[,.N,by=.(calendar_year,model_year,fuel,pollutant )]
+tmp_emfac <- tmp_emfac[N<14,]
+
+extra_sbi <- lapply(1:nrow(tmp_emfac),function(i){ # i = 1
+  get_ef <- emfac[calendar_year == tmp_emfac$calendar_year[i] &
+                    model_year == tmp_emfac$model_year[i] &
+                    fuel == tmp_emfac$fuel[i] &
+                    pollutant == tmp_emfac$pollutant[i]]
+  # number of rep
+  n_rep <- 14 - tmp_emfac$N[i]
+  # speeds
+  ls <- units::set_units(c(106,98,90),"km/h")
+  us <- units::set_units(c(113,105,97),"km/h")
+  # create data.table
+  dt <- data.table::data.table(
+     "calendar_year" = rep(tmp_emfac$calendar_year[i],n_rep)
+    ,"model_year" = rep(tmp_emfac$model_year[i],n_rep)
+    ,"fuel" = rep(tmp_emfac$fuel[i],n_rep)
+    ,"vmt" = rep(units::set_units(0,km),n_rep)
+    ,"ef"= rep(get_ef[.N,ef],n_rep)
+    ,"pollutant" = rep(tmp_emfac$pollutant[i],n_rep)
+    ,"lower_speed_interval" = ls[1:n_rep]
+    , "upper_speed_interval"= us[1:n_rep]
+  )
+  # export dt
+  return(dt)
+})
+extra_sbi <- extra_sbi %>% 
+  data.table::rbindlist()
+
+# add extra_sbi on emfac
+
+emfac_new <- rbind(emfac,extra_sbi)
+
+# order 
+data.table::setkeyv(emfac_new,cols = c("calendar_year"
+                                       ,"model_year"
+                                       ,"fuel"
+                                       ,"pollutant"
+                                       ,"lower_speed_interval"
+                                       ,"upper_speed_interval"))
+
+emfac_new[calendar_year == 2010 & 
+        model_year == 1990 & 
+        pollutant == "NOx" & fuel == "G"]
+
 
 # 7) export-----
 break()
