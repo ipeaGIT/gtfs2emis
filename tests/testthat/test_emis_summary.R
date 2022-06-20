9e3e355d3211275a72f6a2ce5915b98b269d249b
Joao Bazzo <joao.bazzo@gmail.com>
2022-06-20 15:19:55 -0300

    rewrite emis_to_dt and emis_summary tests

--- tests/testthat/test_emis_summary.R
+++ tests/testthat/test_emis_summary.R
@@ -1,71 +1,55 @@
 
 test_that("emis_summary", {
-  # GTFS2gps filter-----
+
   library(data.table)
-  fort <- gtfs2gps::read_gtfs(system.file("extdata/fortaleza.zip"
-                                          , package = "gtfs2gps"))  %>%
-    gtfs2gps::filter_single_trip() %>% 
-    gtfs2gps::filter_by_shape_id(c("shape804-I", "shape806-I"))
+  library(magrittr)
+  library(gtfstools)
+  gtfs_file <- system.file("extdata/irl_dub/irl_dub_gtfs.zip", package = "gtfs2emis")
+  gtfs <- gtfstools::read_gtfs(gtfs_file) %>% 
+    gtfstools::filter_by_trip_id(.
+                                 , trip_id = c('619.3.60-40-d12-1.224.O'))
   
-  fort_gps <- gtfs2gps::gtfs2gps(fort, parallel = TRUE)
-  
-  fort_gpslines <- gtfs2gps::gps_as_sflinestring(fort_gps)
-  
-  fort_gpslines$dist <- units::set_units(fort_gpslines$dist, "km")
+  # Transport model
+  tranp_model <- transport_model(gtfs_data = gtfs,
+                              spatial_resolution = 100,
+                              parallel = TRUE)
   
   # fleet -----------
   total_fleet <- data.table::data.table(year = c(2005,2010,2011,2012
                                                  ,2014,2015,2017,2018,2019),
                                         bus = c(1,61,50,1,45,18,62,27,31),
-                                        veh_type_euro = "Ubus Std 15 - 18 t",
-                                        euro_stage = c("II", "IV", "IV", "V"
-                                                       , "V", "V", "V", "V","V"))
+                                        veh_type = "Ubus Std 15 - 18 t",
+                                        euro = c("II", "IV", "IV", "V"
+                                                       , "V", "V", "V", "V","V")
+                                        , tech = c("-",rep("SCR",8))
+                                        ,fuel = "D")
   
   total_fleet[,fleet_composition := bus/sum(bus)]
   
-  
-  # Ef ----
-  set.seed(123)
-  EF_europe <- ef_emep_europe(pollutant = c("CO", "PM10"),
-                         speed = fort_gpslines$speed,
-                         veh_type = total_fleet$veh_type_euro,
-                         tech = "SCR",
-                         euro = total_fleet$euro_stage,
-                         fcorr = rnorm(9, 0.5, 0.1))
-  # EUROPE
-  emi_europe <- emis(fleet_composition = total_fleet$fleet_composition,
-                     dist = fort_gpslines$dist,
-                     ef = EF_europe,
-                     prefix = "EU"
-                     ,aggregate = FALSE)
-  
+  emi_list <- emission_model(tp_model = tranp_model
+                             , ef_model = "ef_europe_emep"
+                             , fleet_data = total_fleet
+                             , pollutant = c("CO2","PM10"))
   # Expect equal -----
-  summary_time <- emis_summary(emi = emi_europe
-                              ,emi_vars = 'emi'
-                              ,veh_vars = 'veh_type'
-                              ,pol_vars = 'pollutant'
-                              ,by = 'time'
-                              ,time_column = fort_gpslines$timestamp)
-  summary_veh <- emis_summary(emi = emi_europe
-                              ,emi_vars = 'emi'
-                              ,veh_vars = 'veh_type'
-                              ,pol_vars = 'pollutant'
+  summary_time <- emis_summary(emi_list
+                               ,by = 'time'
+                               ,segment_vars = "tp_model"
+                               ,time_column = "timestamp")
+  summary_veh <- emis_summary(emi_list
                               ,by = 'veh_type')
-  summary_pol <- emis_summary(emi = emi_europe
-                              ,emi_vars = 'emi'
-                              ,veh_vars = 'veh_type'
-                              ,pol_vars = 'pollutant'
-                              ,by = 'pollutant')
+  summary_pol <- emis_summary(emi_list)
   
   # Expect equal-----
   expect_equal(ncol(summary_time),3) # ncol
-  expect_equal(ncol(summary_veh),4) # ncol
-  expect_equal(ncol(summary_pol),3) # ncol
-  expect_equal(nrow(summary_time),2) # nrow
-  expect_equal(nrow(summary_veh),62) # nrow
-  expect_equal(nrow(summary_pol),62) # nrow
-  expect_equal(sum(summary_time),11.39935,0.01) # sum(emi)
-  expect_equal(as.numeric(sum(summary_veh$emi,na.rm = TRUE)),19.78512,0.01) # sum(emi)
-  expect_equal(as.numeric(sum(summary_pol$emi,na.rm = TRUE)),19.78512,0.01) # sum(emi)
+  expect_equal(ncol(summary_veh),3) # ncol
+  expect_equal(ncol(summary_pol),2) # ncol
+  expect_equal(nrow(summary_time),10) # nrow
+  expect_equal(nrow(summary_veh),2) # nrow
+  expect_equal(nrow(summary_pol),2) # nrow
+  expect_equal(as.numeric(sum(summary_time$emi)),63985.96,0.01) # sum(emi)
+  expect_equal(as.numeric(sum(summary_veh$emi)),63985.96,0.01) # sum(emi)
+  expect_equal(as.numeric(sum(summary_pol$emi)),63985.96,0.01) # sum(emi)
   
 })
+
+  
--- tests/testthat/test_emis_to_dt.R
+++ tests/testthat/test_emis_to_dt.R
@@ -1,18 +1,7 @@
 
-test_that("emi_to_dt", {
+test_that("emis_to_dt", {
   # GTFS2gps filter-----
-  library(data.table)
-  fort <- gtfs2gps::read_gtfs(system.file("extdata/fortaleza.zip"
-                                          , package = "gtfs2gps"))  %>%
-    gtfs2gps::filter_single_trip() %>% 
-    gtfs2gps::filter_by_shape_id(c("shape804-I", "shape806-I"))
-  
-  fort_gps <- gtfs2gps::gtfs2gps(fort, parallel = TRUE)
-  
-  fort_gpslines <- gtfs2gps::gps_as_sflinestring(fort_gps)
-  
-  fort_gpslines$dist <- units::set_units(fort_gpslines$dist, "km")
-  
+  set.seed(232)
   # fleet -----------
   total_fleet <- data.table::data.table(year = c(2005,2010,2011,2012
                                                  ,2014,2015,2017,2018,2019),
@@ -25,27 +14,26 @@ test_that("emi_to_dt", {
   
   
   # Ef ----
-  set.seed(123)
-  EF_europe <- ef_emep_europe(pollutant = c("CO", "PM10"),
-                         speed = fort_gpslines$speed,
+  EF_europe <- ef_europe_emep(pollutant = c("CO", "PM10"),
+                         speed = units::set_units(seq(1,100,10),"km/h"),
                          veh_type = total_fleet$veh_type_euro,
                          tech = "SCR",
                          euro = total_fleet$euro_stage,
                          fcorr = rnorm(9, 0.5, 0.1))
   
   EF_usa_emfac <- ef_usa_emfac(pollutant = c("CO", "PM10"),
-                               calendar_year = "2019",
+                               reference_year = 2019,
                                model_year = total_fleet$year,
-                               speed = fort_gpslines$speed,
+                               speed = units::set_units(seq(1,100,10),"km/h"),
                                fuel = "D")
   
   EF_usa_moves <- ef_usa_moves(pollutant = c("CO", "PM10"),
-                               calendar_year = "2019",
+                               reference_year = "2019",
                                model_year = total_fleet$year,
-                               speed = fort_gpslines$speed,
+                               speed = units::set_units(seq(1,100,10),"km/h"),
                                fuel = "D")
   
-  EF_brazil <- ef_cetesb_brazil(pollutant = c("CO", "CO2"),
+  EF_brazil <- ef_brazil_cetesb(pollutant = c("CO", "CO2"),
                          veh_type = "BUS_URBAN_D",
                          model_year = total_fleet$year) # fleet_composition
   
@@ -53,45 +41,45 @@ test_that("emi_to_dt", {
   
   # USA
   emi_usa_emfac <- emis(fleet_composition = total_fleet$fleet_composition,
-                        dist = fort_gpslines$dist,
+                        dist = units::set_units(rnorm(10,20,1),"km"),
                         ef = EF_usa_emfac, 
                         prefix = "Emfac"
                         ,aggregate = FALSE)
   emi_usa_moves <- emis(fleet_composition = total_fleet$fleet_composition,
-                        dist = fort_gpslines$dist,
+                        dist = units::set_units(rnorm(10,20,1),"km"),
                         ef = EF_usa_moves, 
                         prefix = "Moves"
                         ,aggregate = FALSE)
   
   # EUROPE
   emi_europe <- emis(fleet_composition = total_fleet$fleet_composition,
-                     dist = fort_gpslines$dist,
+                     dist = units::set_units(rnorm(10,20,1),"km/h"),
                      ef = EF_europe,
                      prefix = "EU"
                      ,aggregate = FALSE)
   
   # BRAZIL (not speed dependent emission factor)
   emi_brazil <- emis(fleet_composition = total_fleet$fleet_composition,
-                     dist = fort_gpslines$dist,
+                     dist = units::set_units(rnorm(10,20,1),"km/h"),
                      ef = EF_brazil,
                      prefix = "BR"
                      ,aggregate = FALSE)
   
   # Expect equal -----
-  emi_dt_europe <- emi_to_dt(emi_list = emi_europe
+  emi_dt_europe <- emis_to_dt(emi_list = emi_europe
                             ,emi_vars = 'emi'
                             ,veh_vars = c("veh_type","euro","fuel","tech")
                             ,pol_vars = 'pollutant'
                             ,segment_vars = c("slope","load")) 
-  emi_dt_brazil <- emi_to_dt(emi_list = emi_brazil
+  emi_dt_brazil <- emis_to_dt(emi_list = emi_brazil
                             ,emi_vars = 'emi'
-                            ,veh_vars = c("years","veh_type")
+                            ,veh_vars = c("model_year","veh_type")
                             ,pol_vars = 'pollutant') 
-  emi_dt_usa_emfac <- emi_to_dt(emi_list = emi_usa_emfac
+  emi_dt_usa_emfac <- emis_to_dt(emi_list = emi_usa_emfac
                             ,emi_vars = 'emi'
                             ,veh_vars = c("model_year","fuel")
                             ,pol_vars = 'pollutant') 
-  emi_dt_usa_moves <- emi_to_dt(emi_list = emi_usa_moves
+  emi_dt_usa_moves <- emis_to_dt(emi_list = emi_usa_moves
                             ,emi_vars = 'emi'
                             ,veh_vars = c("model_year","fuel")
                             ,pol_vars = 'pollutant') 
@@ -101,13 +89,13 @@ expect_equal(ncol(emi_dt_europe),9) # ncol
 expect_equal(ncol(emi_dt_brazil),5) # ncol
 expect_equal(ncol(emi_dt_usa_emfac),5) # ncol
 expect_equal(ncol(emi_dt_usa_moves),5) # ncol
-expect_equal(nrow(emi_dt_europe),558) # nrow
-expect_equal(nrow(emi_dt_brazil),558) # nrow
-expect_equal(nrow(emi_dt_usa_emfac),558) # nrow
-expect_equal(nrow(emi_dt_usa_moves),558) # nrow
-expect_equal(as.numeric(sum(emi_dt_europe$emi,na.rm = TRUE)),19.78512,0.01) # sum(emi)
-expect_equal(as.numeric(sum(emi_dt_brazil$emi,na.rm = TRUE)),19598.96,0.01) # sum(emi)
-expect_equal(as.numeric(sum(emi_dt_usa_emfac$emi,na.rm = TRUE)),2.116568,0.01) # sum(emi)
-expect_equal(as.numeric(sum(emi_dt_usa_moves$emi,na.rm = TRUE)),26.5729,0.01) # sum(emi)
+expect_equal(nrow(emi_dt_europe),180) # nrow
+expect_equal(nrow(emi_dt_brazil),180) # nrow
+expect_equal(nrow(emi_dt_usa_emfac),180) # nrow
+expect_equal(nrow(emi_dt_usa_moves),180) # nrow
+expect_equal(as.numeric(sum(emi_dt_europe$emi,na.rm = TRUE)),217.147,0.01) # sum(emi)
+expect_equal(as.numeric(sum(emi_dt_brazil$emi,na.rm = TRUE)),272501.2,0.01) # sum(emi)
+expect_equal(as.numeric(sum(emi_dt_usa_emfac$emi,na.rm = TRUE)),19.82793,0.01) # sum(emi)
+expect_equal(as.numeric(sum(emi_dt_usa_moves$emi,na.rm = TRUE)),5469.831,0.01) # sum(emi)
 
 })
